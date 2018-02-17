ef146317335162f442b41a1001b0ed9163c1a489
Silvio Henrique Ferreira <shferreira@me.com>
2018-02-17 15:00:19 -0200

    Add Modal

---
+++ app/components/elements/modal.vue
@@ -0,0 +1,17 @@
+<template>
+  <transition enter-class="invisible slideup-ch"
+    leave-to-class="invisible slideup-ch">
+    <div class="fixed fill bg-mask transition" @click.self="$emit('hide')">
+      <div class="transition rounded-bottom shadow p2 bg-white
+        max-width-2 mx-auto will-transform">
+        <slot />
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: "Modal",
+  };
+</script>
--- app/components/pages/app.vue
+++ app/components/pages/app.vue
@@ -3,12 +3,16 @@
     <h1 class="pl3">CMMG-UI</h1>
 
     <TabGroup>
-      <Tab title="Buttons" class="px3">
-        <Btn label="Button" class="btn-primary my2" />
+      <Tab title="Buttons" class="px3 py2">
+        <Btn label="Button" class="btn-primary" />
         <TagGroup v-model="tags" :options="options" class="m2" />
         <ButtonGroup v-model="tag" :options="options" />
       </Tab>
-      <Tab title="Containers" class="px3">
+      <Tab title="Containers" class="px3 py2">
+        <Btn label="Modal" class="btn-primary" @click="modal = true" />
+        <Modal v-if="modal" @hide="modal = false">
+          I'm a modal<br>
+        </Modal>
       </Tab>
       <Tab title="Inputs" class="px3">
       </Tab>
@@ -30,7 +34,9 @@
           { id: 2, name: "Two" },
         ],
         tags: [],
-        tag: null
+        tag: null,
+        page: 1,
+        modal: false,
       };
     },
   };
