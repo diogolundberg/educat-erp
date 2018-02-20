c8ad8c514734bd0b6547c16c614766c470a0edde
Silvio Henrique Ferreira <shferreira@me.com>
2018-02-20 00:54:48 -0300

    Enrollment mockup

--- app/components/pages/app.vue
+++ app/components/pages/app.vue
@@ -3,8 +3,63 @@
     <h1 class="pl3">CMMG-UI</h1>
 
     <TabGroup headerClass="justify-end border-bottom">
-      <Tab title="Home" class="flex flex-wrap">
-        <Fieldset title="Typography" class="col-6 pr2">
+      <Tab title="Matricula">
+        <Stepper class="max-width-3 m-auto">
+          <Step title="Seus Dados">
+            <Card>
+              <Fieldset title="Dados Pessoais">
+                <div class="flex gutters">
+                  <InputBox label="Nome" class="col-12 sm-col-6" />
+                  <InputBox label="CPF" class="col-12 sm-col-3" />
+                  <InputBox label="Nascimento" class="col-12 sm-col-3" />
+                </div>
+                <div class="flex gutters">
+                  <InputBox label="Nacionalidade" class="col-12 sm-col-3" />
+                  <InputBox label="Naturalidade" class="col-12 sm-col-3" />
+                  <InputBox label="Estado Civil" class="col-12 sm-col-3" />
+                  <InputBox label="Sexo" />
+                </div>
+              </Fieldset>
+              <Fieldset title="Dados de Contato">
+                <div class="flex gutters">
+                  <InputBox label="E-mail" class="col-12 sm-col-6" />
+                  <InputBox label="Telefone" class="col-12 sm-col-6" />
+                </div>
+              </Fieldset>
+              <Fieldset title="Endereço">
+                <div class="flex gutters">
+                  <InputBox label="CEP" class="col-12 sm-col-3" />
+                  <InputBox label="Estado" class="col-12 sm-col-3" />
+                  <InputBox label="Cidade" class="col-12 sm-col-3" />
+                  <InputBox label="Tipo de Endereço" class="col-12 sm-col-3" />
+                </div>
+                <div class="flex gutters">
+                  <InputBox label="Logradouro" hint="Rua, Avenida, etc."
+                    class="col-12 sm-col-6" />
+                  <InputBox label="Número" class="col-12 sm-col-3" />
+                  <InputBox label="Complemento" class="col-12 sm-col-3" />
+                </div>
+              </Fieldset>
+              <Fieldset title="Dados para o Censo">
+                <div class="flex gutters">
+                  <InputBox label="Raça" class="col-12 sm-col-3" />
+                  <InputBox label="Escola" class="col-12 sm-col-3" />
+                  <InputBox label="Nome da Escola" class="col-12 sm-col-6" />
+                </div>
+              </Fieldset>
+              <div class="flex justify-end">
+                <Btn label="Enviar para análise" class="btn-primary" />
+              </div>
+            </Card>
+          </Step>
+          <Step title="Aprovação da Secretaria" />
+          <Step title="Dados Financeiros" />
+          <Step title="Aprovação do Financeiro" />
+          <Step title="Matrícula Concluída!" />
+        </Stepper>
+      </Tab>
+      <Tab title="Style Guide" class="flex flex-wrap">
+        <Fieldset title="Typography" class="col-12 sm-col-6 pr2">
           <h1>Heading Level One</h1>
           <h2>Heading Level Two</h2>
           <h3>Heading Level Three</h3>
@@ -58,15 +113,15 @@
         <Fieldset title="Steppers" class="col-6 pr2">
           <Card>
             <Stepper v-model="step">
-              <Step :index="1" title="Step 1" description="My description">
+              <Step title="Step 1" description="My description">
                 <InputBox label="Name" value="" />
                 <Btn label="Continue" class="btn-primary" @click="step = 2" />
               </Step>
-              <Step :index="2" title="Step 2" description="My description">
+              <Step title="Step 2" description="My description">
                 <InputBox label="E-mail" value="" />
                 <Btn label="Continue" class="btn-primary" @click="step = 3" />
               </Step>
-              <Step :index="3" title="Step 3" description="My description">
+              <Step title="Step 3" description="My description">
                 <h3>You're done!</h3>
                 <Btn label="Reset" class="btn-primary" @click="step = 1" />
               </Step>
@@ -82,10 +137,6 @@
           <Progress :value="50" />
         </Fieldset>
       </Tab>
-      <Tab title="Classes" />
-      <Tab title="Students" />
-      <Tab title="Back Office" />
-      <Tab title="Accounts" />
     </TabGroup>
   </div>
 </template>
