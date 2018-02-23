61629f746ce20f6c02529c54d3fb30512a9f09b4
Silvio Henrique Ferreira <shferreira@me.com>
2018-02-22 22:26:29 -0300

    Fix modal z-index bug

--- app/components/elements/modal.vue
+++ app/components/elements/modal.vue
@@ -1,6 +1,6 @@
 <template>
   <transition enter-class="op0" leave-to-class="op0">
-    <div class="fixed fill bg-mask transition" @click.self="$emit('hide')">
+    <div class="fixed fill bg-mask transition z4" @click.self="$emit('hide')">
       <div class="p2 m-auto rounded-bottom shadow2 bg-white max-width-2">
         <slot />
       </div>
