2839756a157bf90c6ae9b8405ff09194d47c1333
Silvio Henrique Ferreira <shferreira@me.com>
2018-02-25 23:30:42 -0300

    Reduce the header shadow

--- app/components/elements/header.vue
+++ app/components/elements/header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header class="fixed top-0 z3 w100 p2 bg-white black shadow1">
+    <header class="fixed top-0 z3 w100 p2 bg-white black shadow0">
       <h1 class="m0 h3">Matrícula</h1>
     </header>
     <div class="p2 mb4"></div>
