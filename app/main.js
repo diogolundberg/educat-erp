fbebda1686a69431cad375f4ba51714705b0b095
Silvio Henrique Ferreira <shferreira@me.com>
2018-02-27 22:31:00 -0300

    Add Router

--- app/main.js
+++ app/main.js
@@ -1,6 +1,15 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import * as Components from "./components/**/*.vue";
 
+Vue.use(VueRouter);
 Object.values(Components).map(a => a.name && Vue.component(a.name, a));
 
-new Vue({ render(h) { return h("App"); } }).$mount("main");
+const router = new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/", component: Vue.component("App") },
+  ],
+});
+
+new Vue({ render: h => h("Application"), router }).$mount("main");
