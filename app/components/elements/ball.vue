0a50c76f65d074029d5f6af71d7a4ab04f35dce9
Silvio Henrique Ferreira <shferreira@me.com>
2018-03-04 18:12:08 -0300

    Abstract away the ball

---
+++ app/components/elements/ball.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    :class="{ 'bg-blue white': !blank, 'shadow0': blank }"
+    class="x2 y2 p4x circle bold h4 center">
+    {{ index }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Ball",
+    props: {
+      index: {
+        type: [Number, String],
+        required: true,
+      },
+      blank: {
+        type: Boolean,
+        default: false,
+        required: false,
+      },
+    },
+  };
+</script>
--- app/components/elements/step.vue
+++ app/components/elements/step.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <div class="flex items-baseline">
-      <div class="mr2 x2 y2 p4x circle bg-blue bold white h4 center shadow1">
-        {{ index }}
-      </div>
+    <div class="flex items-center">
+      <Ball
+        :index="index"
+        class="mr2" />
       <div class="h4">
         {{ title }}
       </div>
