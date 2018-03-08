da38053d47f941b53120cb0fe12f96952897e341
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-03-08 15:57:53 -0300

    add migration to project

---
+++ Migrations/20180308185456_initial.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Onboarding.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AddressTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    CommitedBy = table.Column<string>(nullable: true),
+                    CommittedAt = table.Column<DateTime>(nullable: false),
+                    ExternalId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AddressTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CivilStatus",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    CommitedBy = table.Column<string>(nullable: true),
+                    CommittedAt = table.Column<DateTime>(nullable: false),
+                    ExternalId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CivilStatus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    CommitedBy = table.Column<string>(nullable: true),
+                    CommittedAt = table.Column<DateTime>(nullable: false),
+                    ExternalId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    CommitedBy = table.Column<string>(nullable: true),
+                    CommittedAt = table.Column<DateTime>(nullable: false),
+                    ExternalId = table.Column<int>(nullable: false),
+                    State = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emails", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    CommitedBy = table.Column<string>(nullable: true),
+                    CommittedAt = table.Column<DateTime>(nullable: false),
+                    ExternalId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nationalities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    CommitedBy = table.Column<string>(nullable: true),
+                    CommittedAt = table.Column<DateTime>(nullable: false),
+                    ExternalId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nationalities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PhoneTypes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    CommitedBy = table.Column<string>(nullable: true),
+                    CommittedAt = table.Column<DateTime>(nullable: false),
+                    ExternalId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhoneTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Races",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    CommitedBy = table.Column<string>(nullable: true),
+                    CommittedAt = table.Column<DateTime>(nullable: false),
+                    ExternalId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Races", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schools",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Active = table.Column<bool>(nullable: false),
+                    CommitedBy = table.Column<string>(nullable: true),
+                    CommittedAt = table.Column<DateTime>(nullable: false),
+                    ExternalId = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    State = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schools", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AddressTypes");
+
+            migrationBuilder.DropTable(
+                name: "CivilStatus");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+
+            migrationBuilder.DropTable(
+                name: "Emails");
+
+            migrationBuilder.DropTable(
+                name: "Genders");
+
+            migrationBuilder.DropTable(
+                name: "Nationalities");
+
+            migrationBuilder.DropTable(
+                name: "PhoneTypes");
+
+            migrationBuilder.DropTable(
+                name: "Races");
+
+            migrationBuilder.DropTable(
+                name: "Schools");
+        }
+    }
+}
