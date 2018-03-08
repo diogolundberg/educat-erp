58052363c6d904bd1a19e61aaf910208e8c95a9e
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-03-08 17:51:35 -0300

    change debug port

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace Onboarding
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:5001/")
                 .UseStartup<Startup>()
                 .Build();
     }
