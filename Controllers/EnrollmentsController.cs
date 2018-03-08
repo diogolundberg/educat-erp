c9753359d9b47cb455b134dc64c7be4572293484
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-03-08 15:48:50 -0300

    Organize controllers

---
+++ Controllers/EnrollmentsController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Onboarding.Data.Entity;
+using Onboarding.Models;
+
+namespace Onboarding.Controllers
+{
+    [Route("api/[controller]")]
+    public class EnrollmentsController : Controller
+    {
+        private readonly DatabaseContext _context;
+        private readonly BaseRepository<AddressType> _addressTypeRepository;
+        private readonly BaseRepository<CivilStatus> _civilStatusRepository;
+        private readonly BaseRepository<Country> _countryRepository;
+        private readonly BaseRepository<Gender> _genderRepository;
+        private readonly BaseRepository<Nationality> _nationalityRepository;
+        private readonly BaseRepository<PhoneType> _phoneTypeRepository;
+        private readonly BaseRepository<Race> _RaceRepository;
+
+        public EnrollmentsController(DatabaseContext databaseContext)
+        {
+            _context = databaseContext;
+            _addressTypeRepository = new BaseRepository<AddressType>(_context);
+            _civilStatusRepository = new BaseRepository<CivilStatus>(_context);
+            _countryRepository = new BaseRepository<Country>(_context);
+            _genderRepository = new BaseRepository<Gender>(_context);
+            _nationalityRepository = new BaseRepository<Nationality>(_context);
+            _phoneTypeRepository = new BaseRepository<PhoneType>(_context);
+            _RaceRepository = new BaseRepository<Race>(_context);
+        }
+    }
+}
