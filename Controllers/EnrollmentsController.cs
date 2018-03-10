8151ee660a4a0f741c9c1a84a4193973d6de4f2c
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-03-10 14:06:15 -0300

    add token action

--- Controllers/EnrollmentsController.cs
+++ Controllers/EnrollmentsController.cs
@@ -1,16 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Onboarding.Data.Entity;
 using Onboarding.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace Onboarding.Controllers
 {
-    [Authorize]
     [Route("api/[controller]")]
     public class EnrollmentsController : Controller
     {
+        private readonly IConfiguration _configuration;
         private readonly DatabaseContext _context;
         private readonly BaseRepository<AddressType> _addressTypeRepository;
         private readonly BaseRepository<CivilStatus> _civilStatusRepository;
@@ -21,8 +27,10 @@ namespace Onboarding.Controllers
         private readonly BaseRepository<Race> _RaceRepository;
         private readonly BaseRepository<School> _schoolRepository;
         private readonly BaseRepository<State> _stateRepository;
+        private readonly BaseRepository<Disability> _disabilitiesRepository;
+        private readonly BaseRepository<Enrollment> _enrollmentRepository;
 
-        public EnrollmentsController(DatabaseContext databaseContext)
+        public EnrollmentsController(DatabaseContext databaseContext, IConfiguration configuration)
         {
             _context = databaseContext;
             _addressTypeRepository = new BaseRepository<AddressType>(_context);
@@ -34,16 +42,30 @@ namespace Onboarding.Controllers
             _RaceRepository = new BaseRepository<Race>(_context);
             _schoolRepository = new BaseRepository<School>(_context);
             _stateRepository = new BaseRepository<State>(_context);
+            _disabilitiesRepository = new BaseRepository<Disability>(_context);
+            _enrollmentRepository = new BaseRepository<Enrollment>(_context);
+            _configuration = configuration;
         }
 
-        [HttpGet("")]
-        public dynamic Get ()
+        [HttpGet("{token}")]
+        public dynamic Get (string token)
         {
-            return new { 
-                data = new Enrollment 
-                {
+            if(string.IsNullOrEmpty(token))
+            {
+                return BadRequest();
+            }
 
-                },
+            byte[] data = Convert.FromBase64String(token);
+            string enrollmentTokenJson = Encoding.ASCII.GetString(data);
+            EnrollmentToken enrollmentToken = Newtonsoft.Json.JsonConvert.DeserializeObject<EnrollmentToken>(enrollmentTokenJson);
+
+            if(DateTime.Now >= enrollmentToken.End)
+            {
+                return BadRequest();
+            }
+
+            return new { 
+                data = _enrollmentRepository.GetById(enrollmentToken.Id),
                 options = new 
                 {
                     AddressTypes = _addressTypeRepository.List(),
@@ -54,15 +76,68 @@ namespace Onboarding.Controllers
                     PhoneType = _phoneTypeRepository.List(),
                     Race = _RaceRepository.List(),
                     School = _schoolRepository.List(),
-                    States = new List<State>() { new State { Name = "MG" } }
+                    States = _stateRepository.List(),
+                    Disaries = _disabilitiesRepository.List()
                 }
             };
         }
 
-        [HttpPost("", Name = "ONBOARDING/ENROLLMENTS/ADD")]
-        public IActionResult Create(Enrollment obj)
+        [HttpPatch("{id}", Name = "ONBOARDING/ENROLLMENTS/EDIT")]
+        public IActionResult Update(Guid id, Enrollment obj)
         {
+            if (obj == null || obj.Id != id)
+            {
+                return BadRequest();
+            }
+
+            Enrollment enrollment = _enrollmentRepository.GetById(obj.ExternalId);
+
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            if(enrollment.SendBy.HasValue)
+            {
+                return BadRequest();
+            }
+
+            Enrollment newEnrollment = (Enrollment)enrollment.Clone();
+
+            _enrollmentRepository.Update(enrollment, newEnrollment);
+
             return Ok();
+        }
+
+       [HttpGet("GenerateToken", Name = "SSO/PASSWORD/NEW")]
+        public IActionResult NewPassword(string email)
+        {
+            if(string.IsNullOrEmpty(email))
+            {
+                return BadRequest();
+            }            
+            
+            Enrollment enrollment = new Enrollment { Email = email };
+
+            _enrollmentRepository.Add(enrollment);
+
+            EnrollmentToken enrollmentToken = new EnrollmentToken { Id = enrollment.ExternalId, End = DateTime.Now.AddMonths(1) };
+            string enrollmentTokenJson = Newtonsoft.Json.JsonConvert.SerializeObject(enrollmentToken);
+
+            byte[] bytes = Encoding.ASCII.GetBytes(enrollmentTokenJson);
+            string token = Convert.ToBase64String(bytes.ToArray());
+
+            string emailText = String.Format("?token=" + token);
+            string apiKey = _configuration["SENDGRID_APIKEY"];
+            string plainTextContent = Regex.Replace(emailText, "<[^>]*>", "");
+
+            SendGridClient client = new SendGridClient(apiKey);
+            EmailAddress from = new EmailAddress("suporte@cmmg.com.br", "Suporte");
+            EmailAddress to = new EmailAddress(email);  
+            SendGridMessage msg = MailHelper.CreateSingleEmail(from, to, "subject", plainTextContent, emailText);
+            SendGrid.Response response = client.SendEmailAsync(msg).Result;
+
+            return new OkResult();
         }
     }
 }
