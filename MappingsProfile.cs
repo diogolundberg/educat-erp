fb026492071298be8ee8717460ba4e980e31398d
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-03-15 01:38:15 -0300

    change enrollmentviewmodel mapping

--- MappingsProfile.cs
+++ MappingsProfile.cs
@@ -23,8 +23,28 @@ namespace Onboarding
                 .ForMember(x => x.Race, config => config.Ignore())
                 .ForMember(x => x.School, config => config.Ignore())
                 .ForMember(x => x.EnrollmentDisabilities, config => config.Ignore())
-                .ForMember(x => x.ResponsibleDocumentType, config => config.Ignore())
-                .ForMember(x => x.GuarantorDocumentType, config => config.Ignore())
+                .ForMember(x => x.Guarantor, config => config.MapFrom(cm => new EnrollmentPerson { 
+                                                                                DocumentTypeId = cm.GuarantorDocumentTypeId,
+                                                                                Cpf = cm.GuarantorCpf,
+                                                                                Cnpj = cm.GuarantorCnpj,
+                                                                                Name = cm.GuarantorName,
+                                                                                Contact = cm.GuarantorContact,
+                                                                                Address = cm.GuarantorAddress,
+                                                                                Phone1 = cm.GuarantorPhone1,
+                                                                                Phone2 = cm.GuarantorPhone2,
+                                                                                Email = cm.GuarantorEmail
+                                                                            }))
+                .ForMember(x => x.Responsible, config => config.MapFrom(cm => new EnrollmentPerson {
+                                                                        DocumentTypeId = cm.ResponsibleDocumentTypeId,
+                                                                        Cpf = cm.ResponsibleCpf,
+                                                                        Cnpj = cm.ResponsibleCnpj,
+                                                                        Name = cm.ResponsibleName,
+                                                                        Contact = cm.ResponsibleContact,
+                                                                        Address = cm.ResponsibleAddress,
+                                                                        Phone1 = cm.ResponsiblePhone1,
+                                                                        Phone2 = cm.ResponsiblePhone2,
+                                                                        Email = cm.ResponsibleEmail
+                                                                    }))
                 .ForMember(x => x.EnrollmentDisabilities, config => config
                                                         .MapFrom(cm => 
                                                                     cm.Deficiencies
