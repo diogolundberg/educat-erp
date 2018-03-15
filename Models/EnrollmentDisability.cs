3c432881b4eecb0fb8d00b7e0fa64f9d1928ba3f
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-03-15 01:47:43 -0300

    add jsonignore in EnrollmentDisability

--- Models/EnrollmentDisability.cs
+++ Models/EnrollmentDisability.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 
 namespace Onboarding.Models
 {
@@ -10,12 +11,14 @@ namespace Onboarding.Models
        [Required]
         public Guid DisabilityId { get; set; }
 
+        [JsonIgnore]
         [ForeignKey("DisabilityId")]
         public virtual Disability Disability { get; set; }
 
         [Required]
         public Guid EnrollmentId { get; set; }
 
+        [JsonIgnore]
         [ForeignKey("EnrollmentId")]
         public virtual Enrollment Enrollment { get; set; }
     }    
