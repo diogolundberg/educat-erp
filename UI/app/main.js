6bcb4a4d05379448f7009686121fe8880f915f35
Silvio Henrique Ferreira <shferreira@me.com>
2018-03-20 23:18:17 -0300

    Add autocomplete to the dropdown

--- UI/app/main.js
+++ UI/app/main.js
@@ -5,6 +5,7 @@ import * as Components from "./components/**/*.vue";
 
 Object.values(Components).map(a => a.name && Vue.component(a.name, a));
 
+Vue.directive("el-focus", { inserted: (el) => el.focus() });
 Vue.prototype.sleep = time => new Promise(r => setTimeout(r, time));
 
 new Vue({ render: h => h("Application"), router, store }).$mount("main");
