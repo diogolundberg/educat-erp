29d03e2531a55916386b57d3cebf9455a766bed4
Diogo Lundberg <dclundberg@gmail.com>
2018-03-22 17:34:32 -0300

    Add validations to personaldata view model

--- Onboarding/ViewModel/PersonalDataPatchViewModel.cs
+++ Onboarding/ViewModel/PersonalDataPatchViewModel.cs
@@ -3,95 +3,67 @@ using System.Collections.Generic;
 
 namespace Onboarding.ViewModel
 {
-    public class PersonalDataViewModel
+    public class PersonalDataPatchViewModel
     {
-        [Required]
         public string RealName { get; set; }
 
         public string AssumedName { get; set; }
 
-        [Required]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
-        [Required]
         [Cpf]
         public string CPF { get; set; }
 
-        [Required]
         public string Nationality { get; set; }
 
-        [Required]
         public string HighSchoolGraduationYear { get; set; }
 
-        [Required]
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required]
         public string Zipcode { get; set; }
 
-        [Required]
         public string StreetAddress { get; set; }
 
-        [Required]
         public string ComplementAddress { get; set; }
 
-        [Required]
         public string Neighborhood { get; set; }
 
-        [Required]
         public string PhoneNumber { get; set; }
 
-        [Required]
         public string Landline { get; set; }
 
-        [Required]
         public string MothersName { get; set; }
 
-        [Required]
         public string Handicap { get; set; }
 
-        [Required]
         public string GenderId { get; set; }
 
-        [Required]
         public string MaritalStatusId { get; set; }
 
-        [Required]
         public string BirthCity { get; set; }
 
-        [Required]
         public string BirthStateId { get; set; }
 
-        [Required]
         public string BirthCountryId { get; set; }
 
-        [Required]
         public string HighSchoolGraduationCountryId { get; set; }
 
-        [Required]
         public string City { get; set; }
 
-        [Required]
         public string StateId { get; set; }
 
-        [Required]
         public string AddressKindId { get; set; }
 
-        [Required]
         public string RaceId { get; set; }
 
-        [Required]
         public string HighSchoolKindId { get; set; }
 
-        [Required]
         public IEnumerable<string> Disabilities { get; set; }
 
-        [Required]
         public IEnumerable<string> SpecialNeeds { get; set; }
 
-        [Required]
         public IEnumerable<DocumentViewModel> Documents { get; set; }
     }
 }
