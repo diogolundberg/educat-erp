bef8a3c8327057fcdbf99890682c2a6e9a5d723a
Silvio Henrique Ferreira <shferreira@me.com>
2018-03-22 19:19:35 -0300

    Add snackbar

--- UI/app/main.js
+++ UI/app/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import Snackbar from "snackbarlightjs";
 import router from "./lib/router";
 import store from "./lib/store";
 import * as Components from "./components/**/*.vue";
@@ -7,5 +8,6 @@ Object.values(Components).map(a => a.name && Vue.component(a.name, a));
 
 Vue.directive("el-focus", { inserted: (el) => el.focus() });
 Vue.prototype.sleep = time => new Promise(r => setTimeout(r, time));
+Vue.prototype.notify = msg => Snackbar.create(msg, { timeout: 1000 });
 
 new Vue({ render: h => h("Application"), router, store }).$mount("main");
