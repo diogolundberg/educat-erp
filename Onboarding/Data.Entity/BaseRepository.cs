9e8981c4d953949ab109b1563e241edc171ba62a
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-03-21 21:09:48 -0300

    change baserepository with new keys of basemodel.

--- Onboarding/Data.Entity/BaseRepository.cs
+++ Onboarding/Data.Entity/BaseRepository.cs
@@ -22,7 +22,7 @@ namespace Onboarding.Data.Entity
 
         public virtual IEnumerable<TEntity> List()
         {
-           return _context.Set<TEntity>().Where(x => x.Active);
+            return _context.Set<TEntity>().Where(x => x.Active);
         }
 
         public virtual bool Any()
@@ -30,23 +30,19 @@ namespace Onboarding.Data.Entity
             return _context.Set<TEntity>().Where(x => x.Active).Any();
         }
 
-        public virtual TEntity GetById(int id)
+        public virtual TEntity GetById(string externalId)
         {
-            return _context.Set<TEntity>().FirstOrDefault(x => x.Active && x.ExternalId == id);
+            return _context.Set<TEntity>().FirstOrDefault(x => x.Active && x.ExternalId == externalId);
         }
 
-        public virtual TEntity GetById(Guid id)
+        public virtual TEntity GetById(int id)
         {
             return _context.Set<TEntity>().FirstOrDefault(x => x.Active && x.Id == id);
         }
 
         public virtual void Add(TEntity obj, bool? saveChanges = true)
         {
-            TEntity last = _context.Set<TEntity>().OrderByDescending(x => x.ExternalId).FirstOrDefault();
-            
-            obj.ExternalId =  last != null ? last.ExternalId + 1 : 1;
             obj.DbState = EntityState.Added.ToString();
-            obj.Id = Guid.NewGuid();
 
             _context.Set<TEntity>().Add(obj);
             _context.SaveChanges();
@@ -59,9 +55,10 @@ namespace Onboarding.Data.Entity
 
             _context.SaveChanges();
 
-            newobj.Id = Guid.NewGuid();
+            newobj.Id = 0;
             newobj.ExternalId = obj.ExternalId;
             newobj.DbState = EntityState.Modified.ToString();
+
             _context.Set<TEntity>().Add(newobj);
 
             _context.SaveChanges();
