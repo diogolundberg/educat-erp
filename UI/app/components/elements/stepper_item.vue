68cc8a1e21993eccd29532e9809fb4cdd50673e2
Silvio Henrique Ferreira <shferreira@me.com>
2018-03-24 15:10:25 -0300

    Refactor stepper

--- UI/app/components/elements/stepper_item.vue
+++ UI/app/components/elements/stepper_item.vue
@@ -1,18 +1,21 @@
 <template>
   <div
     :class="{
-      'x2 y2 p6x shadow1': active,
-      'bg-green': complete,
-      'bg-red': error,
+      'x2 y2 p6x shadow1': big,
+      'bg-green': complete && !active,
+      'bg-red': error && !active,
     }"
     class="x1 y1 circle relative"
     @click="$emit('click')">
     <div
-      :class="{ 'bg-green': active, 'bg-gray': !active }"
+      :class="{ 'bg-green': big, 'bg-red': error, 'bg-gray': !big }"
       class="block w100 h100 circle ease">
       <Icon
-        v-if="complete"
+        v-if="complete && !active"
         name="check" />
+      <Icon
+        v-if="error && !active"
+        name="error" />
     </div>
     <div class="absolute left2n x6 pt2 h7 center xs-hide">
       {{ title }}
@@ -40,6 +43,11 @@
         type: String,
         required: false,
         default: null,
+      },
+    },
+    computed: {
+      big() {
+        return this.active || this.complete || this.error;
       },
     },
   };
