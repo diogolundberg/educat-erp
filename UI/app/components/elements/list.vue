42beaf097abcaf72898f1da64bb1c8be59f46870
Silvio Henrique Ferreira <shferreira@me.com>
2018-03-29 01:26:00 -0300

    Add basic List component

---
+++ UI/app/components/elements/list.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="table bg-white shadow2 m2">
+    <div class="table-row divider-bottom">
+      <div
+        v-for="column in columns"
+        :key="column.name"
+        class="table-cell px3 py1">
+        {{ column.title }}
+      </div>
+    </div>
+    <div
+      v-for="(row, index) in value"
+      :key="index"
+      class="table-row divider-bottom">
+      <div
+        v-for="column in columns"
+        :key="column.name"
+        class="table-cell px3 py1">
+        {{ row[column.name] }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "List",
+    props: {
+      value: {
+        type: Array,
+        required: true,
+      },
+      columns: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+</script>
