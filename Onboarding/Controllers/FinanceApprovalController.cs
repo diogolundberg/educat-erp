c97f39240cccf909fb22fa12bd0da7fee5296097
Diogo Lundberg <dclundberg@gmail.com>
2018-03-29 01:43:16 -0300

    Add finance approval list endpoint

---
+++ Onboarding/Controllers/FinanceApprovalController.cs
@@ -0,0 +1,35 @@
+
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Onboarding.Models;
+using Onboarding.ViewModel;
+
+namespace Onboarding.Controllers
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class FinanceApprovalController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly DatabaseContext _context;
+
+        public FinanceApprovalController(DatabaseContext databaseContext, IMapper mapper)
+        {
+            _context = databaseContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet("", Name = "ONBOARDING/FINANCEAPPROVAL/LIST")]
+        public dynamic GetList()
+        {
+            List<Enrollment> enrollments = _context.Enrollments.Include("PersonalData").ToList();
+            List<FinanceApprovalViewModel> approvalList = _mapper.Map<List<FinanceApprovalViewModel>>(enrollments);
+            return new { records = approvalList };
+        }
+    }
+}
