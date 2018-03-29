0859d8eb59e4e45ac93c86d90845cfe152d89ca3
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-03-28 23:19:10 -0300

    Add validations for representative view model.

--- Onboarding/ViewModel/RepresentativeViewModel.cs
+++ Onboarding/ViewModel/RepresentativeViewModel.cs
@@ -5,29 +5,43 @@ namespace Onboarding.ViewModel
 {
     public class RepresentativeViewModel
     {
+        public int? Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string StreetAddress { get; set; }
 
+        [Required]
         public string ComplementAddress { get; set; }
 
+        [Required]
         public string Neighborhood { get; set; }
 
+        [Required]
         public string PhoneNumber { get; set; }
 
+        [Required]
         public string Landline { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
         public int? CityId { get; set; }
 
+        [Required]
         public int? StateId { get; set; }
     }
 
     public class RepresentativeCompanyViewModel : RepresentativeViewModel
     {
+        [Required]
         public string Cnpj { get; set; }
 
+        [Required]
         public string Contact { get; set; }
     }
 
