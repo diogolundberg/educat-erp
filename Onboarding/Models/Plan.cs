365a7736ce67770c44f30779863ba1b0d7d761ed
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-03-29 15:23:22 -0300

    Add plan entity.

---
+++ Onboarding/Models/Plan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Onboarding.Models
+{
+    public class Plan : BaseModel
+    {
+        public string Name { get; set; }
+
+        public  int Installments { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public string Description { get; set; }
+
+        [RegularExpression(@"\d+(\.\d{1,2})?", ErrorMessage = "Valor inválido")]
+        public decimal Value { get; set; }
+    }
+}
