924bca75d8a4b85e745669dfeec532515cd1c03b
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-03-28 22:20:17 -0300

    Create guarantor document.

---
+++ Onboarding/Models/GuarantorDocument.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Onboarding.Models
+{
+    public class GuarantorDocument
+    {
+        [ForeignKey("Guarantor")]
+        public int GuarantorId { get; set; }
+
+        public virtual Guarantor Guarantor { get; set; }
+
+        [ForeignKey("Document")]
+        public int DocumentId { get; set; }
+
+        public virtual Document Document { get; set; }
+    }
+}
