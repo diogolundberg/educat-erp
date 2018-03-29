1b93c01928b788b0e82c64fe730cc762cda289d8
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-03-28 21:32:29 -0300

    Add validations for guarantor view model.

--- Onboarding/ViewModel/GuarantorViewModel.cs
+++ Onboarding/ViewModel/GuarantorViewModel.cs
@@ -1,22 +1,34 @@
-﻿namespace Onboarding.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Onboarding.ViewModel
 {
     public class GuarantorViewModel
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string StreetAddress { get; set; }
 
         public string ComplementAddress { get; set; }
 
+        [Required]
         public string Neighborhood { get; set; }
 
+        [Required]
         public string PhoneNumber { get; set; }
 
+        [Required]
         public string Landline { get; set; }
 
+        [Required]
         public string Email { get; set; }
 
+        [Required]
+
         public int? CityId { get; set; }
+
+        [Required]
 
         public int? StateId { get; set; }
     }
