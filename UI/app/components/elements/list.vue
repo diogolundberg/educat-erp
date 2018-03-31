bb395cb3b45a3fcdc56a034eec2de23e682d0622
Silvio Henrique Ferreira <shferreira@me.com>
2018-03-31 11:05:58 -0300

    Add pagination to the list

--- UI/app/components/elements/list.vue
+++ UI/app/components/elements/list.vue
@@ -1,24 +1,30 @@
 <template>
-  <div class="table bg-white shadow2 m2">
-    <div class="table-row divider-bottom">
+  <div class="m2 flex flex-column items-center">
+    <div class="table bg-white shadow2 m2">
+      <div class="table-row divider-bottom">
+        <div
+          v-for="column in columns"
+          :key="column.name"
+          class="table-cell px4 py2">
+          {{ column.title }}
+        </div>
+      </div>
       <div
-        v-for="column in columns"
-        :key="column.name"
-        class="table-cell px3 py1">
-        {{ column.title }}
+        v-for="(row, index) in rows"
+        :key="index"
+        class="table-row divider-bottom">
+        <div
+          v-for="column in columns"
+          :key="column.name"
+          class="table-cell px4 py2">
+          {{ row[column.name] }}
+        </div>
       </div>
     </div>
-    <div
-      v-for="(row, index) in value"
-      :key="index"
-      class="table-row divider-bottom">
-      <div
-        v-for="column in columns"
-        :key="column.name"
-        class="table-cell px3 py1">
-        {{ row[column.name] }}
-      </div>
-    </div>
+    <Pager
+      v-model="currentPage"
+      :records="value.length"
+      :per-page="perPage" />
   </div>
 </template>
 
@@ -33,6 +39,22 @@
       columns: {
         type: Array,
         required: true,
+      },
+      perPage: {
+        type: Number,
+        required: false,
+        default: 5,
+      },
+    },
+    data() {
+      return {
+        currentPage: 1,
+      };
+    },
+    computed: {
+      rows() {
+        const offset = (this.currentPage - 1) * this.perPage;
+        return this.value.slice(offset, this.perPage + offset);
       },
     },
   };
