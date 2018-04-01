74193133ca7f37747e89285ec7e4fe1ccb4ecc3c
Silvio Henrique Ferreira <shferreira@me.com>
2018-04-01 20:08:18 -0300

    Show sidebar below the header

--- UI/app/components/elements/sidebar.vue
+++ UI/app/components/elements/sidebar.vue
@@ -3,7 +3,7 @@
     enter-class="left0"
     leave-to-class="left0">
     <aside
-      class="fixed top-0 bottom-0 left-0 z4 bg-white shadow2 ease width-1">
+      class="fixed top-0 bottom-0 left-0 z2 mt4 bg-white shadow2 ease width-1">
       <div class="relative bg-green-grad">
         <div>
           <img
@@ -13,15 +13,13 @@
         <div class="white px2 py1 truncate">{{ login }}</div>
       </div>
       <div>
-        <div class="h-bg-silver">
-          <a
-            v-for="link in links"
-            :key="link.url"
-            :href="link.url"
-            class="block p2 bold black truncate text-decoration-none">
-              {{ link.name }}
-          </a>
-        </div>
+        <router-link
+          v-for="(link,index) in links"
+          :key="index"
+          :to="link.url"
+          class="block p2 bold black truncate text-decoration-none h-bg-silver">
+            {{ link.name }}
+        </router-link>
       </div>
     </aside>
   </transition>
