5ccf40e8810298960e8202327f536ec52d901cce
Silvio Henrique Ferreira <shferreira@me.com>
2018-04-01 15:06:32 -0300

    Add BaseErrors component

---
+++ UI/app/components/elements/base_errors.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Alert
+      v-for="error in value"
+      :key="error"
+      :message="error"
+      error />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "BaseErrors",
+    props: {
+      value: {
+        type: Array,
+        default: () => [],
+      },
+    },
+  };
+</script>
