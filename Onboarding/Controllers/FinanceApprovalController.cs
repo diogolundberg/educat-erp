94ffd5e08d118cf1dfae3450424c9322e21b50c7
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-01 20:56:19 -0300

    Fix bug list without cpf and name.

--- Onboarding/Controllers/FinanceApprovalController.cs
+++ Onboarding/Controllers/FinanceApprovalController.cs
@@ -28,7 +28,7 @@ namespace Onboarding.Controllers
         [HttpGet("", Name = "ONBOARDING/FINANCEAPPROVAL/LIST")]
         public dynamic GetList()
         {
-            List<Enrollment> enrollments = _context.Enrollments.Include("FinanceData").Where(x => x.SentAt.HasValue).ToList();
+            List<Enrollment> enrollments = _context.Enrollments.Include("PersonalData").Where(x => x.SentAt.HasValue).ToList();
             List<Records> records = _mapper.Map<List<Records>>(enrollments);
             return new { records };
         }
