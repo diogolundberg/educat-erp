992e2121ccffeed587988ff1fe51ac317c1db537
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-01 20:09:02 -0300

    Create migration for entity enrollment pendency.

--- Onboarding/Migrations/20180401223425_fix_pendencies.Designer.cs
+++ Onboarding/Migrations/20180401223425_fix_pendencies.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180401223425_fix_pendencies")]
+    partial class fix_pendencies
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Onboarding/Migrations/20180401223425_fix_pendencies.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Onboarding.Migrations
+{
+    public partial class fix_pendencies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pendencies_Enrollments_EnrollmentId",
+                table: "Pendencies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pendencies_EnrollmentId",
+                table: "Pendencies");
+
+            migrationBuilder.DropColumn(
+                name: "EnrollmentId",
+                table: "Pendencies");
+
+            migrationBuilder.CreateTable(
+                name: "EnrollmentPendency",
+                columns: table => new
+                {
+                    EnrollmentId = table.Column<int>(nullable: false),
+                    PendencyId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnrollmentPendency", x => new { x.EnrollmentId, x.PendencyId });
+                    table.ForeignKey(
+                        name: "FK_EnrollmentPendency_Enrollments_EnrollmentId",
+                        column: x => x.EnrollmentId,
+                        principalTable: "Enrollments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EnrollmentPendency_Pendencies_PendencyId",
+                        column: x => x.PendencyId,
+                        principalTable: "Pendencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnrollmentPendency_PendencyId",
+                table: "EnrollmentPendency",
+                column: "PendencyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EnrollmentPendency");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EnrollmentId",
+                table: "Pendencies",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pendencies_EnrollmentId",
+                table: "Pendencies",
+                column: "EnrollmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pendencies_Enrollments_EnrollmentId",
+                table: "Pendencies",
+                column: "EnrollmentId",
+                principalTable: "Enrollments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
