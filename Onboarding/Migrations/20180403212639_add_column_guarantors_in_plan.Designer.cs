e39a00b7db42e7fdf740fdf8c977e35f3c90b0d0
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-03 18:28:35 -0300

    Add column guarantors  in plan.

--- Onboarding/Migrations/20180403212639_add_column_guarantors_in_plan.Designer.cs
+++ Onboarding/Migrations/20180403212639_add_column_guarantors_in_plan.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180403212639_add_column_guarantors_in_plan")]
+    partial class add_column_guarantors_in_plan
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
