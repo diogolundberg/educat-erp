5dfc959dbbd6c288aeb93d65eadf132574885feb
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-04 16:11:44 -0300

    Create enrollment messages validator. #58

---
+++ Onboarding/Validations/EnrollmentMessagesValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Onboarding.Models;
+
+namespace Onboarding.Validations
+{
+    public class EnrollmentMessagesValidator : AbstractValidator<Enrollment>
+    {
+        public EnrollmentMessagesValidator(DatabaseContext databaseContext)
+        {
+
+        }
+    }
+}
