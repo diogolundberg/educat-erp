8db0d93f0624c56fe4f39f2bdaeab58f46f9e23d
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-04 20:14:17 -0300

    Add migration for property photo into enrollment.

--- Onboarding/Migrations/20180404231343_add_photo_into_enrollment.Designer.cs
+++ Onboarding/Migrations/20180404231343_add_photo_into_enrollment.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180404231343_add_photo_into_enrollment")]
+    partial class add_photo_into_enrollment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Onboarding/Migrations/20180404231343_add_photo_into_enrollment.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Onboarding.Migrations
+{
+    public partial class add_photo_into_enrollment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Enrollments",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Enrollments");
+        }
+    }
+}
