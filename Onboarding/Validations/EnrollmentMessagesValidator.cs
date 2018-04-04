741fafda8a0effa62dc9e824e36f011f265283c2
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-04 17:23:47 -0300

    Set validations for finance data validator and personal data validator. #58

--- Onboarding/Validations/EnrollmentMessagesValidator.cs
+++ Onboarding/Validations/EnrollmentMessagesValidator.cs
@@ -8,25 +8,10 @@ namespace Onboarding.Validations
     {
         public EnrollmentMessagesValidator(DatabaseContext databaseContext)
         {
-            RuleFor(enrollment => enrollment).Custom((enrollment, context) =>
-            {
-                databaseContext.Entry(enrollment).Reference(x => x.FinanceData).Load();
-                databaseContext.Entry(enrollment.FinanceData).Reference(x => x.Plan).Load();
-                databaseContext.Entry(enrollment.FinanceData).Collection(x => x.Guarantors).Load();
-
-                CheckPlan(enrollment, context);
-            });
+            RuleFor(enrollment => enrollment.FinanceData).SetValidator(new FinanceDataMessagesValidator(databaseContext));
+            RuleFor(enrollment => enrollment.PersonalData).SetValidator(new PersonalDataMessagesValidator(databaseContext));
         }
 
-        private void CheckPlan(Enrollment enrollment, CustomContext context)
-        {
-            if (enrollment.FinanceData.Plan != null && enrollment.FinanceData.Plan.Guarantors > 0)
-            {
-                if (enrollment.FinanceData.Plan.Guarantors > enrollment.FinanceData.Guarantors.Count)
-                {
-                    context.AddFailure(string.Format("Para o plano {0} é necessário preencher {1} fiador(es)", enrollment.FinanceData.Plan.Name, enrollment.FinanceData.Plan.Guarantors));
-                }
-            }
-        }
+
     }
 }
