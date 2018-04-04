d39b1239ecfc22d9e3864517a33781bb4f193b03
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-04 10:12:39 -0300

    Add column check military draft into gender.

--- Onboarding/Migrations/20180404130309_add_column_checkmilitarydraft_into_gender.Designer.cs
+++ Onboarding/Migrations/20180404130309_add_column_checkmilitarydraft_into_gender.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180404130309_add_column_checkmilitarydraft_into_gender")]
+    partial class add_column_checkmilitarydraft_into_gender
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Onboarding/Models/Gender.cs
+++ Onboarding/Models/Gender.cs
@@ -1,15 +1,12 @@
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Onboarding.Models
 {
     public class Gender : BaseModel
     {
-        public Gender ()
-        {
-        }
-
         [Required]
         public string Name { get; set; }
+
+        public bool CheckMilitaryDraft { get; set; }
     }
 }
