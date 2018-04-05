9c7e01848f5589cac59f1afc3b2f51d56cd13015
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-05 12:09:22 -0300

    Create list for post onboarding. #61

--- Onboarding/ViewModels/Onboarding/Form.cs
+++ Onboarding/ViewModels/Onboarding/Form.cs
@@ -1,6 +1,13 @@
-﻿namespace Onboarding.ViewModels.Onboarding
+﻿using System.Collections.Generic;
+
+namespace Onboarding.ViewModels.Onboarding
 {
     public class Form
+    {
+        public List<Item> Items { get; set; }
+    }
+
+    public class Item
     {
         public string Name { get; set; }
         public string Cpf { get; set; }
