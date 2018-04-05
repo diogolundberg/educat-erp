62ae31c2db860542c0a299311d9b077fba65ad59
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-05 11:39:05 -0300

    Add view model for onboarding post. #61

---
+++ Onboarding/ViewModels/Onboarding/Form.cs
@@ -0,0 +1,9 @@
+﻿namespace Onboarding.ViewModels.Onboarding
+{
+    public class Form
+    {
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+    }
+}
