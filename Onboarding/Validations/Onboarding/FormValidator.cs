605038c3851f5fb5e3fa4d1c98c4629bc1a3c00c
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-05 11:40:02 -0300

    Add validation for view model onboarding post. #61

---
+++ Onboarding/Validations/Onboarding/FormValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Onboarding.Validations.Onboarding
+{
+    public class FormValidator : AbstractValidator<ViewModels.Onboarding.Form>
+    {
+        public FormValidator()
+        {
+            RuleFor(x => x.Name).NotNull();
+            RuleFor(x => x.Email).NotNull().EmailAddress();
+            RuleFor(x => x.Cpf).Must(x => Cpf.ValidCPF(x));
+        }
+    }
+}
