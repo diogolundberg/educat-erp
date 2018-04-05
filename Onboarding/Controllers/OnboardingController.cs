64adcc449e80838748ac091512959df469e5f26e
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-05 12:39:36 -0300

    Change onboarding endpoint. #61

--- Onboarding/Controllers/OnboardingController.cs
+++ Onboarding/Controllers/OnboardingController.cs
@@ -1,15 +1,36 @@
-﻿using FluentValidation.Results;
+﻿using AutoMapper;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Onboarding.Models;
 using Onboarding.Validations.Onboarding;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Globalization;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace Onboarding.Controllers
 {
     [Route("api/[controller]")]
     public class OnboardingController : BaseController
     {
+        private readonly IMapper _mapper;
+        private readonly DatabaseContext _context;
+        private readonly IConfiguration _configuration;
+
+        public OnboardingController(DatabaseContext databaseContext, IMapper mapper, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _context = databaseContext;
+            _mapper = mapper;
+        }
+
         [HttpPost("", Name = "ONBOARDING/CREATE")]
         public dynamic Post([FromBody]ViewModels.Onboarding.Form obj)
         {
+            List<Enrollment> enrollments = new List<Enrollment>();
             FormValidator formValidator = new FormValidator();
             ValidationResult validationResult = formValidator.Validate(obj);
 
@@ -18,7 +39,62 @@ namespace Onboarding.Controllers
                 return new BadRequestObjectResult(new { Errors = FormatErrors(validationResult) });
             }
 
-            return new OkObjectResult(new { data = obj });
+            foreach (var item in obj.Items)
+            {
+                Enrollment enrollment = new Enrollment
+                {
+                    Deadline = DateTime.ParseExact(obj.Deadline, "dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    PersonalData = new PersonalData
+                    {
+                        RealName = item.Name,
+                        Email = item.Email,
+                        CPF = item.Cpf,
+                    },
+                    FinanceData = new FinanceData
+                    {
+                        Representative = new RepresentativePerson()
+                    }
+                };
+
+                string externalId = enrollment.CreateExternalId();
+
+                Enrollment existingEnrollment = _context.Enrollments.Include("PersonalData").SingleOrDefault(x => x.ExternalId == externalId);
+
+                if (existingEnrollment == null)
+                {
+                    _context.Enrollments.Add(enrollment);
+                    _context.SaveChanges();
+
+                    SendEmail(enrollment.ExternalId, enrollment.PersonalData.Email);
+                }
+                else
+                {
+                    enrollment = existingEnrollment;
+                }
+
+                enrollments.Add(enrollment);
+            }
+
+            return new OkObjectResult(new
+            {
+                data = enrollments.Select(x => new
+                {
+                    token = x.ExternalId,
+                    name = x.PersonalData.RealName,
+                    email = x.PersonalData.Email,
+                    cpf = x.PersonalData.CPF
+                })
+            });
+        }
+
+        private void SendEmail(string externalId, string email)
+        {
+            SmtpClientHelper smtpClientHelper = new SmtpClientHelper(_configuration["SMTP_PORT"], _configuration["SMTP_HOST"], _configuration["SMTP_USERNAME"], _configuration["SMTP_PASSWORD"]);
+
+            string body = string.Format("Clique <a href='{0}'>aqui</a> para se matricular", _configuration["ENROLLMENT_HOST"] + externalId);
+            string subject = _configuration["EMAIL_ENROLLMENTS_SUBJECT"];
+
+            smtpClientHelper.Send(new MailAddress(_configuration["EMAIL_SENDER_ONBOARDING"]), new MailAddress(email), body, subject);
         }
     }
 }
