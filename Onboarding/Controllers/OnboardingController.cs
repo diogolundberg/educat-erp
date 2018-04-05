bdb3e10486d9167d7b1b984c95059066a14a8e87
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-05 12:09:46 -0300

    Add onboarding endpoint. #61

---
+++ Onboarding/Controllers/OnboardingController.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Onboarding.Validations.Onboarding;
+
+namespace Onboarding.Controllers
+{
+    [Route("api/[controller]")]
+    public class OnboardingController : BaseController
+    {
+        [HttpPost("", Name = "ONBOARDING/CREATE")]
+        public dynamic Post([FromBody]ViewModels.Onboarding.Form obj)
+        {
+            FormValidator formValidator = new FormValidator();
+            ValidationResult validationResult = formValidator.Validate(obj);
+
+            if (!validationResult.IsValid)
+            {
+                return new BadRequestObjectResult(new { Errors = FormatErrors(validationResult) });
+            }
+
+            return new OkObjectResult(new { data = obj });
+        }
+    }
+}
