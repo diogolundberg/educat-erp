0eb5f0b2c683703af7af11300af657b00146bc70
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-06 06:54:56 -0300

    Add migration for onboarding.

--- Onboarding/Migrations/20180406095423_add_more_properties_onboarding.Designer.cs
+++ Onboarding/Migrations/20180406095423_add_more_properties_onboarding.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180406095423_add_more_properties_onboarding")]
+    partial class add_more_properties_onboarding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
