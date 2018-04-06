6bd57d367641d36efe254ed67dcf54b79199f99e
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-06 06:48:18 -0300

    Change view models for enrollment.

---
+++ Onboarding/ViewModels/Onboarding/EnrollmentForm.cs
@@ -0,0 +1,9 @@
+﻿namespace Onboarding.ViewModels.Onboarding
+{
+    public class EnrollmentForm
+    {
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+    }
+}
--- Onboarding/ViewModels/Onboarding/Form.cs
+++ Onboarding/ViewModels/Onboarding/Form.cs
@@ -4,16 +4,9 @@ namespace Onboarding.ViewModels.Onboarding
 {
     public class Form
     {
-        public List<Item> Items { get; set; }
-        public string Deadline { get; set; }
+        public List<EnrollmentForm> Enrollments { get; set; }
+        public string StartAt { get; set; }
         public string Year { get; set; }
         public string Semester { get; set; }
-    }
-
-    public class Item
-    {
-        public string Name { get; set; }
-        public string Cpf { get; set; }
-        public string Email { get; set; }
     }
 }
