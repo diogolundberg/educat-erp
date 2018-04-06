fd015c095b102c7920ef1c06f7ad1cae10f4e2d9
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-06 10:29:44 -0300

    Add columns for view model finance approval. #47

--- Onboarding/ViewModels/FinanceApprovals/Records.cs
+++ Onboarding/ViewModels/FinanceApprovals/Records.cs
@@ -7,5 +7,17 @@ namespace Onboarding.ViewModels.FinanceApprovals
         public string CPF { get; set; }
 
         public string EnrollmentNumber { get; set; }
+
+        public string BirthDate { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string UpdatedAt { get; set; }
+
+        public string Plan { get; set; }
+
+        public string State { get; set; }
     }
 }
