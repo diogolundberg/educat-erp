b712611e1841593bb72defc5474c1d59f5cd871d
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-06 10:30:13 -0300

    Fix mapping for finance approval. #47

--- Onboarding/Bindings/FinanceApprovalProfile.cs
+++ Onboarding/Bindings/FinanceApprovalProfile.cs
@@ -12,7 +12,13 @@ namespace Onboarding.Bindings
             CreateMap<Enrollment, Records>()
             .ForMember(x => x.Name, config => config.MapFrom(x => x.PersonalData.RealName))
             .ForMember(x => x.EnrollmentNumber, config => config.MapFrom(x => x.ExternalId))
-            .ForMember(x => x.CPF, config => config.MapFrom(x => x.PersonalData.CPF));
+            .ForMember(x => x.CPF, config => config.MapFrom(x => x.PersonalData.CPF))
+            .ForMember(x => x.BirthDate, config => config.MapFrom(x => x.PersonalData.BirthDate.Value.ToString("dd/MM/yyyy")))
+            .ForMember(x => x.Email, config => config.MapFrom(x => x.PersonalData.Email))
+            .ForMember(x => x.PhoneNumber, config => config.MapFrom(x => x.PersonalData.PhoneNumber))
+            .ForMember(x => x.UpdatedAt, config => config.MapFrom(x => x.FinanceData.UpdatedAt))
+            .ForMember(x => x.Plan, config => config.MapFrom(x => x.FinanceData.Plan.Name))
+            .ForMember(x => x.PhoneNumber, config => config.MapFrom(x => x.PersonalData.PhoneNumber));
 
             CreateMap<Enrollment, Record>()
             .ForMember(x => x.Name, config => config.MapFrom(x => x.PersonalData.RealName))
