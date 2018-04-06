ec432577b65b58c310fd0fb8136044a4316ac3f1
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-06 08:48:17 -0300

    Add migration for fix name of startat into enrollment.

--- Onboarding/Migrations/20180406114732_fix_name_startat_into_enrollment.Designer.cs
+++ Onboarding/Migrations/20180406114732_fix_name_startat_into_enrollment.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180406114732_fix_name_startat_into_enrollment")]
+    partial class fix_name_startat_into_enrollment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
