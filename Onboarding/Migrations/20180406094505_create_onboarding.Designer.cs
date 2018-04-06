c404acb274ceb2fc12cbb8fec34a8779acf72aa6
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-06 06:45:28 -0300

    Add migration for onboarding.

--- Onboarding/Migrations/20180406094505_create_onboarding.Designer.cs
+++ Onboarding/Migrations/20180406094505_create_onboarding.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180406094505_create_onboarding")]
+    partial class create_onboarding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Onboarding/Migrations/20180406094505_create_onboarding.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Onboarding.Migrations
+{
+    public partial class create_onboarding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OnboardingId",
+                table: "Enrollments",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Onboardings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    EndAt = table.Column<DateTime>(nullable: true),
+                    ExternalId = table.Column<string>(nullable: true),
+                    StartAt = table.Column<DateTime>(nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Onboardings", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollments_OnboardingId",
+                table: "Enrollments",
+                column: "OnboardingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Enrollments_Onboardings_OnboardingId",
+                table: "Enrollments",
+                column: "OnboardingId",
+                principalTable: "Onboardings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Enrollments_Onboardings_OnboardingId",
+                table: "Enrollments");
+
+            migrationBuilder.DropTable(
+                name: "Onboardings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Enrollments_OnboardingId",
+                table: "Enrollments");
+
+            migrationBuilder.DropColumn(
+                name: "OnboardingId",
+                table: "Enrollments");
+        }
+    }
+}
