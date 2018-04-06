35bbbe1aeac65ddca10209e64def902ccabacf4d
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-06 07:24:25 -0300

    Create mapper for onboarding.

---
+++ Onboarding/Bindings/OnboardingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System.Linq;
+
+namespace Onboarding.Bindings
+{
+    public class OnboardingProfile : Profile
+    {
+        public OnboardingProfile()
+        {
+            CreateMap<ViewModels.Onboarding.Form, Models.Onboarding>()
+            .ForMember(x => x.Enrollments, config => config.MapFrom(x => x.Enrollments.Select(o => new Models.Enrollment
+            {
+                PersonalData = new Models.PersonalData
+                {
+                    RealName = o.Name,
+                    CPF = o.CPF,
+                    Email = o.Email
+                }
+            })));
+        }
+    }
+}
