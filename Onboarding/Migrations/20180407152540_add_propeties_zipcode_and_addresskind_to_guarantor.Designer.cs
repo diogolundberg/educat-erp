f714c227f30d5a8292bdd1142287efd2955277a9
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-07 12:27:47 -0300

    Add zipcode and adress kind to guarantors.

--- Onboarding/Migrations/20180407152540_add_propeties_zipcode_and_addresskind_to_guarantor.Designer.cs
+++ Onboarding/Migrations/20180407152540_add_propeties_zipcode_and_addresskind_to_guarantor.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180407152540_add_propeties_zipcode_and_addresskind_to_guarantor")]
+    partial class add_propeties_zipcode_and_addresskind_to_guarantor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Onboarding/Migrations/20180407152540_add_propeties_zipcode_and_addresskind_to_guarantor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Onboarding.Migrations
+{
+    public partial class add_propeties_zipcode_and_addresskind_to_guarantor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AddressKindId",
+                table: "Guarantors",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Zipcode",
+                table: "Guarantors",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guarantors_AddressKindId",
+                table: "Guarantors",
+                column: "AddressKindId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Guarantors_AddressKinds_AddressKindId",
+                table: "Guarantors",
+                column: "AddressKindId",
+                principalTable: "AddressKinds",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Guarantors_AddressKinds_AddressKindId",
+                table: "Guarantors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Guarantors_AddressKindId",
+                table: "Guarantors");
+
+            migrationBuilder.DropColumn(
+                name: "AddressKindId",
+                table: "Guarantors");
+
+            migrationBuilder.DropColumn(
+                name: "Zipcode",
+                table: "Guarantors");
+        }
+    }
+}
