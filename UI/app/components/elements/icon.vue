9c03f8963028007c257aa86408d735aba40de748
Silvio Henrique Ferreira <shferreira@me.com>
2018-04-07 16:21:11 -0300

    Update hamburguer menu

--- UI/app/components/elements/icon.vue
+++ UI/app/components/elements/icon.vue
@@ -1,8 +1,14 @@
 <template>
   <span :class="{ 'fill-white': white, 'fill-black': black }">
     <svg
-      v-if="name === 'menu'">
-      <path d="M6 36h36v-4h-36v4zm0-10h36v-4h-36v4zm0-14v4h36v-4h-36z"/>
+      v-if="name === 'menu'"
+      :width="width"
+      viewBox="2 2 30 25">
+      <path
+        d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,
+        8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2 s0.896,2,2,2h24c1.104,0,
+        2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,
+        2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" />
     </svg>
     <svg
       v-if="name === 'error'"
