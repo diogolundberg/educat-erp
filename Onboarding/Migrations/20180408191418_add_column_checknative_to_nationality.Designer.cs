e715403cab5234eb836457d1f31c3dcc488b8ab6
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-08 16:50:29 -0300

    Add migration for create column check native.

--- Onboarding/Migrations/20180408191418_add_column_checknative_to_nationality.Designer.cs
+++ Onboarding/Migrations/20180408191418_add_column_checknative_to_nationality.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180408191418_add_column_checknative_to_nationality")]
+    partial class add_column_checknative_to_nationality
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Onboarding/Migrations/20180408191418_add_column_checknative_to_nationality.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Onboarding.Migrations
+{
+    public partial class add_column_checknative_to_nationality : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CheckNative",
+                table: "Nationalities",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckNative",
+                table: "Nationalities");
+        }
+    }
+}
