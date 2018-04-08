f5dddd6b97facba95098ff834d284f7a173f75fe
Silvio Henrique Ferreira <shferreira@me.com>
2018-04-08 20:54:15 -0300

    Start the form component

---
+++ UI/app/components/elements/data_form.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2
+      v-if="title">
+      {{ title }}
+    </h2>
+    <slot
+      :item="item"
+      :errors="errors" />
+    <div class="right-align">
+      <Btn
+        submit
+        primary
+        label="Salvar"
+        @click="submitForm" />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "DataForm",
+    props: {
+      title: {
+        type: String,
+        required: false,
+        default: null,
+      },
+    },
+    data() {
+      return {
+        item: {},
+        errors: {},
+      };
+    },
+    mounted() {
+
+    },
+    methods: {
+      submitForm() {
+
+      },
+    },
+  };
+</script>
