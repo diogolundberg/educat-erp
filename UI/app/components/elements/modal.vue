80f01ecd07f223844a75718c18d2bde5e92c8296
Silvio Henrique Ferreira <shferreira@me.com>
2018-04-08 17:33:28 -0300

    Enlarge the modal window

--- UI/app/components/elements/modal.vue
+++ UI/app/components/elements/modal.vue
@@ -3,9 +3,9 @@
       enter-class="op0 scale0"
       leave-to-class="op0 scale0">
     <div
-      class="fixed fill bg-dim ease flex z4"
+      class="fixed fill bg-dim ease flex z4 overflow-scroll"
       @click.self="$emit('hide')">
-      <div class="p2 m-auto rounded shadow2 bg-white width-2 bounce">
+      <div class="p2 m-auto rounded shadow2 bg-white width-4 bounce">
         <slot />
       </div>
     </div>
