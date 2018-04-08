aace443940586af3d3b90d5f163ac6418ff8de07
Silvio Henrique Ferreira <shferreira@me.com>
2018-04-07 21:37:18 -0300

    Add standalone radio component

---
+++ UI/app/components/elements/radio.vue
@@ -0,0 +1,22 @@
+<template>
+  <span
+    :aria-checked="value"
+    class="inline-block align-middle x1 y1 p4x circle bg-white shadow0"
+    role="radio">
+    <template v-if="value">
+      <div class="block w100 h100 circle bg-blue ease" />
+    </template>
+  </span>
+</template>
+
+<script>
+  export default {
+    name: "Radio",
+    props: {
+      value: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  };
+</script>
