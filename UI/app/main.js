c932dd1f6b085ebe15512aa7d39b4fd6a8208944
Silvio Henrique Ferreira <shferreira@me.com>
2018-04-08 23:48:00 -0300

    Increase the toast display time

--- UI/app/main.js
+++ UI/app/main.js
@@ -9,6 +9,6 @@ Object.values(Components).map(a => a.name && Vue.component(a.name, a));
 Vue.filter("date", (v) => new Date(v).toLocaleDateString());
 Vue.directive("el-focus", { inserted: (el) => el.focus() });
 Vue.prototype.sleep = time => new Promise(r => setTimeout(r, time));
-Vue.prototype.notify = msg => Snackbar.create(msg, { timeout: 1000 });
+Vue.prototype.notify = msg => Snackbar.create(msg, { timeout: 5000 });
 
 new Vue({ render: h => h("Application"), router, store }).$mount("main");
