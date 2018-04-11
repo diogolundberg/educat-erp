47e390d70a1c075f325cd7b380074b1f6a0f8814
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-11 06:35:45 -0300

    Add migration for transform datetime for string. #87

--- Onboarding/Migrations/20180411093146_transform_all_datetimes_for_varchar.Designer.cs
+++ Onboarding/Migrations/20180411093146_transform_all_datetimes_for_varchar.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180411093146_transform_all_datetimes_for_varchar")]
+    partial class transform_all_datetimes_for_varchar
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Onboarding/Migrations/20180411093146_transform_all_datetimes_for_varchar.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Onboarding.Migrations
+{
+    public partial class transform_all_datetimes_for_varchar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BirthDate",
+                table: "PersonalDatas",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StartAt",
+                table: "Onboardings",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EndAt",
+                table: "Onboardings",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StartedAt",
+                table: "Enrollments",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SentAt",
+                table: "Enrollments",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewedAt",
+                table: "Enrollments",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FinanceApproval",
+                table: "Enrollments",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AcademicApproval",
+                table: "Enrollments",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "PersonalDatas",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartAt",
+                table: "Onboardings",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndAt",
+                table: "Onboardings",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartedAt",
+                table: "Enrollments",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SentAt",
+                table: "Enrollments",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReviewedAt",
+                table: "Enrollments",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FinanceApproval",
+                table: "Enrollments",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AcademicApproval",
+                table: "Enrollments",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
