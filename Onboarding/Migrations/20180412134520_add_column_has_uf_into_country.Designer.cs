94c5882cb3e30377970db99ae09d047f05d59a80
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-12 10:46:00 -0300

    Add migration for create has uf into country. #76

--- Onboarding/Migrations/20180412134520_add_column_has_uf_into_country.Designer.cs
+++ Onboarding/Migrations/20180412134520_add_column_has_uf_into_country.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180412134520_add_column_has_uf_into_country")]
+    partial class add_column_has_uf_into_country
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Onboarding/Migrations/20180412134520_add_column_has_uf_into_country.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Onboarding.Migrations
+{
+    public partial class add_column_has_uf_into_country : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasUF",
+                table: "Countries",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasUF",
+                table: "Countries");
+        }
+    }
+}
