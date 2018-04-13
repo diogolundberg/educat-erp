41e125ebf01912f5539b48a515e109e746ccef21
Silvio Henrique Ferreira <shferreira@me.com>
2018-04-13 00:37:28 -0300

    Add enrollment info

--- UI/app/components/pages/enrollment.vue
+++ UI/app/components/pages/enrollment.vue
@@ -404,6 +404,60 @@
           </Card>
         </Step>
         <Step
+          title="Informações da Matrícula"
+          description="Saiba mais sobre seu curso">
+          <Card
+            title="Informações da Matrícula">
+            <Fieldset title="Informações Gerais">
+              <InfoBox
+                label="Curso"
+                value="Medicina" />
+              <InfoBox
+                label="Turno"
+                value="Integral" />
+              <InfoBox
+                label="Forma de Ingresso"
+                value="Vestibular" />
+              <InfoBox
+                label="Data de Ingresso"
+                value="01/02/2018" />
+            </Fieldset>
+            <Fieldset title="Matriz Curricular">
+              <List
+                :show-filter="false"
+                :per-page="40"
+                :columns="[
+                  {name:'series', title: 'Série'},
+                  {name:'class', title: 'Disciplinas', css: 'col-5'},
+                  {name:'theory', title: 'Teórica'},
+                  {name:'practice', title: 'Prática'},
+                  {name:'total', title: 'Total'},
+                ]"
+                :value="[
+                  {series:'1',class:'Anatomia I',theory:80,practice:80,total:160},
+                  {series:'1',class:'Anatomia II',theory:80,practice:80,total:160},
+                  {series:'1',class:'Biofísica I',theory:40,practice:'-',total:40},
+                  {series:'1',class:'Biofísica II',theory:72,practice:'-',total:72},
+                  {series:'1',class:'Bioquímica I',theory:80,practice:'-',total:80},
+                  {series:'1',class:'Bioquímica II',theory:40,practice:40,total:80},
+                  {series:'1',class:'Citologia',theory:80,practice:80,total:160},
+                  {series:'1',class:'Embriologia Humana',theory:80,practice:80,total:160},
+                  {series:'1',class:'Genética',theory:72,practice:72,total:144},
+                  {series:'1',class:'Histologia I',theory:40,practice:40,total:80},
+                  {series:'1',class:'Histologia I',theory:40,practice:40,total:80},
+                  {series:'1',class:'História da Medicina I',theory:72,practice:72,
+                   total:144},
+                  {series:'1',class:'Medicina Preventiva I',theory:80,practice:80,
+                   total:160},
+                  {series:'1',class:'Metodologia Científica',theory:80,practice:80,
+                   total:160},
+                  {series:'1',class:'Práticas em Saúde Coletiva',theory:72,practice:72,
+                   total:144},
+              ]" />
+            </Fieldset>
+          </Card>
+        </Step>
+        <Step
           v-if="!enrollment.data.sentAt"
           title="Enviar para Análise"
           description="Enviar para aprovação da secretaria e departamento
