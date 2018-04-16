5e734c86fc88dea0950bf8cce14c5f9a2ee28b27
Diogo Lundberg <dclundberg@gmail.com>
2018-04-16 10:31:30 -0300

    Add Finance data status

---
+++ Onboarding/Statuses/BaseStatus.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace Onboarding.Statuses
+{
+    public abstract class BaseStatus<T>
+    {
+        public BaseStatus(AbstractValidator<T> validator)
+        {
+
+        }
+    }
+}
---
+++ Onboarding/Statuses/FinanceDataStatus.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Onboarding.Models;
+
+namespace Onboarding.Statuses
+{
+    public class FinanceDataStatus : BaseStatus<FinanceData>
+    {
+
+    }
+}
