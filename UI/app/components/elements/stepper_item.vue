16e315b71d31b03325429fd0f720457badca22d4
Silvio Henrique Ferreira <shferreira@me.com>
2018-04-20 03:36:47 -0300

    Show step warning status in stepper header

--- UI/app/components/elements/stepper_item.vue
+++ UI/app/components/elements/stepper_item.vue
@@ -4,11 +4,17 @@
       'x2 y2 p6x shadow1': big,
       'bg-green': complete && !active,
       'bg-red': error && !active,
+      'bg-yellow': warning && !active,
     }"
     class="x1 y1 circle relative"
     @click="$emit('click')">
     <div
-      :class="{ 'bg-green': big, 'bg-red': error, 'bg-gray': !big }"
+      :class="{
+        'bg-green': big,
+        'bg-red': error,
+        'bg-gray': !big,
+        'bg-yellow': warning,
+      }"
       class="block w100 h100 circle ease">
       <Icon
         v-if="complete && !active"
@@ -16,6 +22,9 @@
       <Icon
         v-if="error && !active"
         name="error" />
+      <Icon
+        v-if="warning && !active"
+        name="warning" />
     </div>
     <div class="absolute left2n x6 pt2 h7 center xs-hide">
       {{ title }}
@@ -39,6 +48,10 @@
         type: Boolean,
         default: false,
       },
+      warning: {
+        type: Boolean,
+        default: false,
+      },
       title: {
         type: String,
         required: false,
@@ -47,7 +60,7 @@
     },
     computed: {
       big() {
-        return this.active || this.complete || this.error;
+        return this.active || this.complete || this.error || this.warning;
       },
     },
   };
