fc20047afd429b96fd7ae36444f2b682dbcb62e7
Silvio Henrique Ferreira <shferreira@me.com>
2018-04-23 20:45:34 -0300

    Add alt-text to key-value fields

--- UI/app/components/elements/key_value.vue
+++ UI/app/components/elements/key_value.vue
@@ -5,7 +5,9 @@
     <div class="h6">
       {{ title }}:
     </div>
-    <div class="h4 truncate">
+    <div
+      :title="value"
+      class="h4 truncate">
       {{ value }}
     </div>
   </div>
