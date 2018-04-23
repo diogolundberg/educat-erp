11b24086cd443c0f351a763cfafc7a0f0a4848c0
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-04-23 16:08:11 -0300

    Add guarantors resources. #110

--- Onboarding/Validations/FinanceData/GuarantorValidator.cs
+++ Onboarding/Validations/FinanceData/GuarantorValidator.cs
@@ -16,10 +16,9 @@ namespace Onboarding.Validations
             RuleFor(guarantor => guarantor.Neighborhood).NotEmpty().WithName(onboarding.Resources.Models.Guarantor.Neighborhood);
             RuleFor(guarantor => guarantor.PhoneNumber).NotEmpty().WithName(onboarding.Resources.Models.Guarantor.PhoneNumber);
             RuleFor(guarantor => guarantor.Landline).NotEmpty().WithName(onboarding.Resources.Models.Guarantor.Landline);
-            RuleFor(guarantor => guarantor.Email).NotEmpty().WithName(onboarding.Resources.Models.Guarantor.Email);
             RuleFor(guarantor => guarantor.CityId).NotEmpty().WithName(onboarding.Resources.Models.Guarantor.CityId);
             RuleFor(guarantor => guarantor.StateId).NotEmpty().WithName(onboarding.Resources.Models.Guarantor.StateId);
-            RuleFor(guarantor => guarantor.FinanceDataId).NotEmpty().WithName(onboarding.Resources.Models.Guarantor.FinanceDataId);
+            RuleFor(guarantor => guarantor.FinanceDataId).NotEmpty();
             RuleFor(guarantor => guarantor.RelationshipId).NotEmpty().WithName(onboarding.Resources.Models.Guarantor.RelationshipId);
             RuleFor(guarantor => guarantor.AddressKindId).NotEmpty().WithName(onboarding.Resources.Models.Guarantor.AddressKindId);
             RuleFor(guarantor => guarantor.Zipcode).NotEmpty().WithName(onboarding.Resources.Models.Guarantor.Zipcode);
