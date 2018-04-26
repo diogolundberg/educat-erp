eecde82dab7233b227af6cdb790eda405b74e2ab
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-04-26 14:39:42 -0300

    Create viewmodel form for scheduling. #114

---
+++ Onboarding/ViewModels/Scheduling/Form.cs
@@ -0,0 +1,17 @@
+﻿namespace onboarding.ViewModels.Scheduling
+{
+    public class Form
+    {
+        public int OnboardingId { get; set; }
+        public string StartAt { get; set; }
+        public string EndAt { get; set; }
+        public string Intervals { get; set; }
+        public List<FormBreak> Breaks { get; set; }
+    }
+
+    public class FormBreak
+    {
+        public string Start { get; set; }
+        public string End { get; set; }
+    }
+}
