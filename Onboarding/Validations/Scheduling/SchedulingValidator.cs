644d2128d1eab462449363a402cc4cc892e958ec
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-04-26 17:58:57 -0300

    Add validation for intervals into scheduling. #114

--- Onboarding/Validations/Scheduling/SchedulingValidator.cs
+++ Onboarding/Validations/Scheduling/SchedulingValidator.cs
@@ -5,13 +5,26 @@ namespace onboarding.Validations.Scheduling
 {
     public class SchedulingValidator : AbstractValidator<Models.Scheduling>
     {
-        public SchedulingValidator(DatabaseContext context)
+        public SchedulingValidator(DatabaseContext databaseContext)
         {
             RuleFor(scheduling => scheduling.OnboardingId).NotNull().WithName(Resources.Models.Scheduling.OnboardingId);
             RuleFor(scheduling => scheduling.StartAt).NotNull().WithName(Resources.Models.Scheduling.StartAt);
             RuleFor(scheduling => scheduling.EndAt).NotNull().WithName(Resources.Models.Scheduling.EndAt);
             RuleFor(scheduling => scheduling.Intervals).NotNull().WithName(Resources.Models.Scheduling.Intervals);
-            RuleFor(scheduling => scheduling).Must(x => x.EndAt >= x.StartAt).WithPropertyName("StartAt").WithMessage(Resources.Models.Scheduling.StartAtGreaterThanEndAt);
+            RuleFor(scheduling => scheduling.Intervals).Custom((intervals, context) =>
+            {
+                if (!string.IsNullOrEmpty(intervals))
+                {
+                    int convertedValue = 0;
+                    int.TryParse(intervals, out convertedValue);
+
+                    if (convertedValue == 0)
+                    {
+                        context.AddFailure("Intervals", string.Format(Resources.Shared.NotValidField, Resources.Models.Scheduling.Intervals));
+                    }
+                }
+            });
+            RuleFor(scheduling => scheduling).Must(x => x.EndAt >= x.StartAt).WithMessage(Resources.Models.Scheduling.StartAtGreaterThanEndAt);
         }
     }
 }
