59973124d1cae880375b81632dfa7ce9e99233c8
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-04-26 17:02:26 -0300

    Add extension for format datetime type.

--- Onboarding/DateTimeHelper.cs
+++ Onboarding/DateTimeHelper.cs
@@ -11,5 +11,10 @@ namespace onboarding
         {
             return date.HasValue ? date.Value.ToString("dd/MM/yyyy") : null;
         }
+
+        public static string Format(this DateTime date)
+        {
+            return date.ToString("dd/MM/yyyy");
+        }
     }
 }
