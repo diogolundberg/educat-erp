9efb0c3760fa911277fdba1405d3ca589481ae22
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-04-26 17:24:48 -0300

    Add record to scheduling. #114

---
+++ Onboarding/ViewModels/Scheduling/Record.cs
@@ -0,0 +1,10 @@
+﻿namespace onboarding.ViewModels.Scheduling
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string Onboarding { get; set; }
+        public string StartAt { get; set; }
+        public string EndAt { get; set; }
+    }
+}
