79543b3523d38237b681dd27e38c835b45fd5760
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-04-26 16:47:50 -0300

    Add scheduling profile. #114

---
+++ Onboarding/Bindings/SchedulingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using onboarding.Models;
+using onboarding.ViewModels.Scheduling;
+
+namespace onboarding.Bindings
+{
+    public class SchedulingProfile : Profile
+    {
+        public SchedulingProfile()
+        {
+            CreateMap<Scheduling, Form>()
+                .ForMember(x => x.Breaks, config => config.MapFrom(x => x.FormBreaks));
+
+            CreateMap<Form, Scheduling>()
+                .ForMember(x => x.Breaks, config => config.MapFrom(x => JsonConvert.SerializeObject(x.Breaks)));
+        }
+    }
+}
