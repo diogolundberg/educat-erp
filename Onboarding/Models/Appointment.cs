f36189254249f11958089b6cd207c64cf7e460b1
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-04-27 13:40:33 -0300

    Add appointment class. #121

---
+++ Onboarding/Models/Appointment.cs
@@ -0,0 +1,12 @@
+﻿namespace onboarding.Models
+{
+    public class Appointment
+    {
+        public int? EnrollmentId { get; set; }
+        public Enrollment Enrollment { get; set; }
+        public string Date { get; set; }
+        public string Hour { get; set; }
+        public int SchedulingId { get; set; }
+        public Scheduling Scheduling { get; set; }
+    }
+}
