eff5d66727c9372f09d9521eb1a7548fd5b96609
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-04-27 13:53:59 -0300

    Add migration for appointments. #121

--- Onboarding/Migrations/20180427165129_add_table_appointment.Designer.cs
+++ Onboarding/Migrations/20180427165129_add_table_appointment.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180427165129_add_table_appointment")]
+    partial class add_table_appointment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Onboarding/Migrations/20180427165129_add_table_appointment.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace onboarding.Migrations
+{
+    public partial class add_table_appointment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    Date = table.Column<string>(nullable: true),
+                    EnrollmentId = table.Column<int>(nullable: true),
+                    ExternalId = table.Column<string>(nullable: true),
+                    Hour = table.Column<string>(nullable: true),
+                    SchedulingId = table.Column<int>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Enrollments_EnrollmentId",
+                        column: x => x.EnrollmentId,
+                        principalTable: "Enrollments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Schedulings_SchedulingId",
+                        column: x => x.SchedulingId,
+                        principalTable: "Schedulings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_EnrollmentId",
+                table: "Appointments",
+                column: "EnrollmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_SchedulingId",
+                table: "Appointments",
+                column: "SchedulingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+        }
+    }
+}
