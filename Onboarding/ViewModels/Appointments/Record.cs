e4a5ced6b6e3b160dd84167ea51c2b479c6859c2
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-04-30 21:42:50 -0300

    Add property for record appointment view model. #121

--- Onboarding/ViewModels/Appointments/Record.cs
+++ Onboarding/ViewModels/Appointments/Record.cs
@@ -2,6 +2,7 @@
 {
     public class Record
     {
+        public int Id { get; set; }
         public int EnrollmentId { get; set; }
         public string Date { get; set; }
         public string Hour { get; set; }
