ef265a0f6989eca1f946154af75335cbb9fd32b7
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-04-30 21:23:09 -0300

    Add appointment view model. #121

---
+++ Onboarding/ViewModels/Appointments/Record.cs
@@ -0,0 +1,10 @@
+﻿namespace onboarding.ViewModels.Appointments
+{
+    public class Record
+    {
+        public int EnrollmentId { get; set; }
+        public string Date { get; set; }
+        public string Hour { get; set; }
+        public int SchedulingId { get; set; }
+    }
+}
