a05620919319f3c2eb36547c46e6aed29fee086b
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-03 11:48:39 -0300

    Create database context and base model. #117

---
+++ invoice/Models/Base/BaseModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace invoice.Models
+{
+    public class BaseModel : ICloneable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        public string ExternalId { get; set; }
+
+        [JsonIgnore]
+        public DateTime? CreatedAt { get; set; }
+
+        [JsonIgnore]
+        public DateTime? UpdatedAt { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public virtual string CreateExternalId()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
