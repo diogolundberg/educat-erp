75d000898ed2ffd1f4725fbe9361aad1eb71d868
Silvio Henrique Ferreira <shferreira@me.com>
2018-05-06 16:42:59 -0300

    Add an onboarding filter to the schedule list

--- UI/app/components/pages/scheduling_list.vue
+++ UI/app/components/pages/scheduling_list.vue
@@ -8,7 +8,17 @@
         {name: 'startAt', title: 'Inicio'},
         {name: 'endAt', title: 'Fim'},
       ]"
-      @click="show($event)" />
+      :default-filters="defaultFilters"
+      @click="show($event)">
+      <template slot-scope="{ filters }">
+        <DropDown
+          v-model="filters.onboarding"
+          :options="onboardingList"
+          class="ml1"
+          id-field="name"
+          label="Onboarding" />
+      </template>
+    </List>
     <div class="right m2">
       <router-link
         to="/scheduling/new"
@@ -22,12 +32,26 @@
 <script>
   export default {
     name: "SchedulingList",
+    params: {
+      onboarding: {
+        type: String,
+        default: null,
+      },
+    },
     computed: {
       schedulingList() {
         return this.$store.state.schedulingList;
       },
+      onboardingList() {
+        return this.$store.state.onboardings;
+      },
+      defaultFilters() {
+        const { onboarding } = this.$route.query;
+        return onboarding && { onboarding };
+      },
     },
     async mounted() {
+      await this.$store.dispatch("getOnboardingList");
       await this.$store.dispatch("getSchedulingList");
     },
     methods: {
