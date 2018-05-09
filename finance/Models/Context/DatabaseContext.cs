9d15e20c392935a8311a6ae7257bf3f91e0f0220
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-09 17:12:32 -0300

    Add nuget packages.

---
+++ finance/Models/Context/DatabaseContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace finance.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var auditableEntity in ChangeTracker.Entries<BaseModel>())
+            {
+                auditableEntity.Entity.ExternalId = !string.IsNullOrEmpty(auditableEntity.Entity.ExternalId) ? auditableEntity.Entity.ExternalId : auditableEntity.Entity.CreateExternalId();
+
+                if (auditableEntity.State == EntityState.Added)
+                {
+                    auditableEntity.Entity.CreatedAt = DateTime.Now;
+                }
+                else if (auditableEntity.State == EntityState.Modified)
+                {
+                    auditableEntity.Property(x => x.CreatedAt).IsModified = false;
+                    auditableEntity.Entity.UpdatedAt = DateTime.Now;
+                }
+            }
+
+            return base.SaveChanges();
+        }
+    }
+}
