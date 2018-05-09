3b7eb203597d00fa59e87f70e7376fb9ceda96af
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-09 10:51:09 -0300

    Remove enrollmentid into record appointment.

--- Onboarding/ViewModels/Appointments/Record.cs
+++ Onboarding/ViewModels/Appointments/Record.cs
@@ -3,7 +3,6 @@
     public class Record
     {
         public int Id { get; set; }
-        public int EnrollmentId { get; set; }
         public string Date { get; set; }
         public string Hour { get; set; }
         public int SchedulingId { get; set; }
