a5cfff8c20a340712abcb32e635c481103eb23b7
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-10 15:21:49 -0300

    Add get list invoice route.

--- finance/Controllers/InvoicesController.cs
+++ finance/Controllers/InvoicesController.cs
@@ -1,8 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using finance.Models;
+using finance.ViewModels.invoices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace finance.Controllers
 {
-    public class InvoicesController : BaseController
+    public class InvoicesController : BaseController 
     {
+        private readonly IMapper _mapper;
+        private readonly DatabaseContext _context;
+
+        public InvoicesController(DatabaseContext databaseContext, IMapper mapper)
+        {
+            _context = databaseContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet("", Name = "FINANCE/INVOICES/LIST")]
+        public dynamic GetList()
+        {
+            return _mapper.Map<List<Records>>(_context.Invoices.Include("InvoiceItens"));
+        }
+
     }
 }
