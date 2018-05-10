6bf796115a2017e30bc0ae8fd7efd82c004dee77
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-10 16:03:11 -0300

    Add invoice validator.

---
+++ finance/Validations/InvoiceValidator.cs
@@ -0,0 +1,15 @@
+﻿using finance.Models;
+using FluentValidation;
+
+namespace finance.Validations
+{
+    public class InvoiceValidator : AbstractValidator<Models.Invoice>
+    {
+        public InvoiceValidator(DatabaseContext databaseContext)
+        {
+            RuleFor(invoice => invoice.InvoiceNumber).NotNull().WithName(Resources.Models.Invoice.InvoiceNumber);
+            RuleFor(invoice => invoice.Value).NotNull().WithName(Resources.Models.Invoice.Value);
+            RuleFor(invoice => invoice.DueDate).NotNull().WithName(Resources.Models.Invoice.DueDate);
+        }
+    }
+}
