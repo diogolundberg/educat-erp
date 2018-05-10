c7d21126073492ce8a5104e241ac01a89566903a
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-10 15:21:33 -0300

    Create invoice profile.

---
+++ finance/Bindings/InvoicesProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using finance.Models;
+using finance.ViewModels.invoices;
+
+namespace finance.Bindings
+{
+    public class InvoicesProfile : Profile
+    {
+        public InvoicesProfile()
+        {
+            CreateMap<Invoice, Records>().ReverseMap();
+        }
+    }
+}
