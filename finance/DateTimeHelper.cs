dd308ea8be97caebffae90c536770ce5b36514c2
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-10 15:30:43 -0300

    Add datetime helper into finance api.

---
+++ finance/DateTimeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace finance
+{
+    static class DateTimeHelper
+    {
+        public static string Format(this DateTime? date)
+        {
+            return date.HasValue ? date.Value.ToString("dd/MM/yyyy") : null;
+        }
+
+        public static string Format(this DateTime date)
+        {
+            return date.ToString("dd/MM/yyyy");
+        }
+    }
+}
