3f6f7c4b93c0f48ff5ded5d6956bd5bbaa50aec9
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-10 15:21:21 -0300

    Add invoice view models.

---
+++ finance/ViewModels/Invoices/Record.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace onboarding.ViewModels.Scheduling
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string Onboarding { get; set; }
+        public int OnboardingId { get; set; }
+        public string StartAt { get; set; }
+        public string EndAt { get; set; }
+        public string ScheduleStartTime { get; set; }
+        public string ScheduleEndTime { get; set; }
+        public string Intervals { get; set; }
+        public List<FormBreak> Breaks { get; set; }
+    }
+}
---
+++ finance/ViewModels/Invoices/Records.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace finance.ViewModels.invoices
+{
+    public class Records
+    {
+        public int Id { get; set; }
+        public string InvoiceNumber { get; set; }
+        public decimal Value { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
