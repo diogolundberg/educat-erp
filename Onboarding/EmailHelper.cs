89af3bbe42a6e3cd22dc92caf21580d8edcfb150
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-22 17:46:38 -0300

    Create param for check request isLocal.

--- Onboarding/EmailHelper.cs
+++ Onboarding/EmailHelper.cs
@@ -4,14 +4,17 @@ namespace onboarding
 {
     public class EmailHelper
     {
-        public void SendEmail(string messageBody, string subject, string from, string to, string userName, string password)
+        public void SendEmail(string messageBody, string subject, string from, string to, string userName, string password, bool isLocal)
         {
             string smtpHost = "smtp.sendgrid.net";
             string smtpPort = "587";
 
             SmtpClientHelper smtpClientHelper = new SmtpClientHelper(smtpPort, smtpHost, userName, password);
 
-            smtpClientHelper.Send(new MailAddress(from), new MailAddress(to), messageBody, subject);
+            if (!isLocal)
+            {
+                smtpClientHelper.Send(new MailAddress(from), new MailAddress(to), messageBody, subject);
+            }
         }
     }
 }
