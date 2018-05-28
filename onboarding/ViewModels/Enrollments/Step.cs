3e466e1a5c1ea1c5e98c079267e5db20773bdd3f
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-28 13:30:32 -0300

    Create step viewmodel.

---
+++ onboarding/ViewModels/Enrollments/Step.cs
@@ -0,0 +1,9 @@
+﻿namespace onboarding.ViewModels.Enrollments
+{
+    public class Step
+    {
+        public string Name { get; set; }
+        public string Resource { get; set; }
+        public string Status { get; set; }
+    }
+}
