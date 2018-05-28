4c76a97bbe3945e2b065929d28a129e0b8ea8b1f
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-28 13:14:28 -0300

    Add relationship for enrollment step into step.

--- onboarding/Models/Step.cs
+++ onboarding/Models/Step.cs
@@ -1,9 +1,18 @@
-﻿namespace onboarding.Models
+﻿using System.Collections.Generic;
+
+namespace onboarding.Models
 {
     public class Step : BaseModel
     {
+        public Step()
+        {
+            EnrollmentSteps = new HashSet<EnrollmentStep>();
+        }
+
         public string Name { get; set; }
         public string Resource { get; set; }
         public int Order { get; set; }
+
+        public ICollection<EnrollmentStep> EnrollmentSteps { get; set; }
     }
 }
