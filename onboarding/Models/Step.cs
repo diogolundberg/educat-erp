7249596b48725708cc890fe9cabbfda4b7d6467c
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-28 13:04:42 -0300

    Add step class.

---
+++ onboarding/Models/Step.cs
@@ -0,0 +1,9 @@
+﻿namespace onboarding.Models
+{
+    public class Step : BaseModel
+    {
+        public string Name { get; set; }
+        public string Resource { get; set; }
+        public int Order { get; set; }
+    }
+}
