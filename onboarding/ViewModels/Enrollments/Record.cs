84ad843ff406cd1c75c455e2e74c23777e153304
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-28 13:31:12 -0300

    Add step view model into record enrollment.

--- onboarding/ViewModels/Enrollments/Record.cs
+++ onboarding/ViewModels/Enrollments/Record.cs
@@ -6,12 +6,12 @@ namespace onboarding.ViewModels.Enrollments
     {
         public Record()
         {
-            Cards = new HashSet<Card>();
+            Steps = new HashSet<Step>();
         }
 
         public string DaysRemaining { get; set; }
         public string OnboardingYear { get; set; }
         public string Photo { get; set; }
-        public IEnumerable<Card> Cards { get; set; }
+        public IEnumerable<Step> Steps { get; set; }
     }
 }
