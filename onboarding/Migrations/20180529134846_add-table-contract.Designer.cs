76b0ce44e9e75bb48e653381522696275f5c3ab5
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-29 10:49:47 -0300

    Add migration for create contract.

--- onboarding/Migrations/20180529134846_add-table-contract.Designer.cs
+++ onboarding/Migrations/20180529134846_add-table-contract.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180529134846_add-table-contract")]
+    partial class addtablecontract
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ onboarding/Migrations/20180529134846_add-table-contract.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace onboarding.Migrations
+{
+    public partial class addtablecontract : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contracts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AcceptedAt = table.Column<DateTime>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    EnrollmentId = table.Column<int>(nullable: true),
+                    ExternalId = table.Column<string>(nullable: true),
+                    Signature = table.Column<string>(nullable: true),
+                    URL = table.Column<string>(nullable: true),
+                    UpdatedAt = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contracts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contracts_Enrollments_EnrollmentId",
+                        column: x => x.EnrollmentId,
+                        principalTable: "Enrollments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contracts_EnrollmentId",
+                table: "Contracts",
+                column: "EnrollmentId",
+                unique: true,
+                filter: "[EnrollmentId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contracts");
+        }
+    }
+}
