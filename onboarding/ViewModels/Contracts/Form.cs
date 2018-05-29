6954917d2073ef9bfdec8bbba60025ba6f48be56
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-29 12:24:46 -0300

    Add form view model for contract.

---
+++ onboarding/ViewModels/Contracts/Form.cs
@@ -0,0 +1,9 @@
+﻿namespace onboarding.ViewModels.Contracts
+{
+    public class Form
+    {
+        public int EnrollmentId { get; set; }
+        public string Signature { get; set; }
+        public bool Accept { get; set; }
+    }
+}
