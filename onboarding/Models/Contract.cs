d6132868c3df419365f67d09dbc52fe3ccc550e1
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-29 10:49:18 -0300

    Add contract class.

---
+++ onboarding/Models/Contract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace onboarding.Models
+{
+    public class Contract : BaseModel
+    {
+        [ForeignKey("Enrollment")]
+        public int? EnrollmentId { get; set; }
+        public virtual Enrollment Enrollment { get; set; }
+
+        public string URL { get; set; }
+        public string Signature { get; set; }
+        public DateTime AcceptedAt { get; set; }
+    }
+}
