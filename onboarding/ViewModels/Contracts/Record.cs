7b40c839dcc1bd1ebcadaa331b4ae1046d94a885
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-29 15:28:50 -0300

    Add content for record contract.

--- onboarding/ViewModels/Contracts/Record.cs
+++ onboarding/ViewModels/Contracts/Record.cs
@@ -4,6 +4,7 @@
     {
         public int? EnrollmentId { get; set; }
         public string URL { get; set; }
+        public string Content { get; set; }
         public string Signature { get; set; }
         public string AcceptedAt { get; set; }
     }
