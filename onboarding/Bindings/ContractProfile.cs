933f1f043e2f60a830e5420dfada48093ab7e621
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-29 11:46:44 -0300

    Add contract profile.

---
+++ onboarding/Bindings/ContractProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using onboarding.Models;
+using onboarding.ViewModels.Contracts;
+
+namespace onboarding.Bindings
+{
+    public class ContractProfile : Profile
+    {
+        public ContractProfile()
+        {
+            CreateMap<Contract, Record>()
+            .ForMember(x => x.AcceptedAt, config => config.MapFrom(x => x.AcceptedAt.Format()));
+        }
+    }
+}
