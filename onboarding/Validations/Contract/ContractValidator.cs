d329b8e551e88cd37372d840a9ffa924bba406c6
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-29 12:25:37 -0300

    Add contract validator.

---
+++ onboarding/Validations/Contract/ContractValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using onboarding.Models;
+
+namespace onboarding.Validations.Contract
+{
+    public class ContractValidator : AbstractValidator<Models.Contract>
+    {
+
+        public ContractValidator()
+        {
+            RuleFor(contract => contract.AcceptedAt).NotEmpty().WithName(onboarding.Resources.Models.Contract.AcceptedAt);
+            RuleFor(contract => contract.Signature).NotEmpty().WithName(onboarding.Resources.Models.Contract.Signature);
+        }
+    }
+}
