52b36524478647644a9948ba5208d253832df265
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-01 16:26:55 -0300

    Add properties Id to approval records.

--- onboarding/ViewModels/Approvals/Records.cs
+++ onboarding/ViewModels/Approvals/Records.cs
@@ -2,6 +2,7 @@
 {
     public class Records
     {
+        public int Id { get; set; }
         public string SentAt { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
