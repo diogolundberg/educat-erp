4f88530331ee1f5d62ec13eff086fd4dcca4ae9d
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-01 15:51:08 -0300

    Remove unnecesary propeties into enrollment.

--- onboarding/Migrations/20180601185031_remove_unnecessary_properties_into_enrollment.Designer.cs
+++ onboarding/Migrations/20180601185031_remove_unnecessary_properties_into_enrollment.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180601185031_remove_unnecessary_properties_into_enrollment")]
+    partial class remove_unnecessary_properties_into_enrollment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
