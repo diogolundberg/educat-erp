9694793c80197b888e6f4f835fe58402cf6e4be6
Silvio Henrique Ferreira <shferreira@me.com>
2018-06-02 19:41:54 -0300

    Add big and info option to the badges

--- ui/app/components/elements/badge.vue
+++ ui/app/components/elements/badge.vue
@@ -1,7 +1,13 @@
 <template>
   <span
-    :class="{ 'bg-green': success, 'bg-red': error, 'bg-yellow': warning }"
-    class="rounded shadow1 white bg-blue bold btn-small mr1">
+    :class="{
+      'bg-green': success,
+      'bg-red': error,
+      'bg-yellow': warning,
+      'bg-blue': info,
+      'rounded50 px4 upcase': big,
+    }"
+    class="rounded shadow1 bg-gray white truncate bold btn-small mr1">
     <slot />
   </span>
 </template>
@@ -21,6 +27,16 @@
         required: false,
       },
       warning: {
+        type: Boolean,
+        default: false,
+        required: false,
+      },
+      info: {
+        type: Boolean,
+        default: false,
+        required: false,
+      },
+      big: {
         type: Boolean,
         default: false,
         required: false,
