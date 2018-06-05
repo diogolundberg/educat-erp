9a30b8cf20cbe1b35d461355b3179ae57b89c689
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-05 14:12:48 -0300

    Add pendency profile.

---
+++ onboarding/Bindings/PendencyProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace onboarding.Bindings
+{
+    public class PendencyProfile : Profile
+    {
+        public PendencyProfile()
+        {
+            CreateMap<Models.Pendency, ViewModels.Pendency>().ReverseMap();
+        }
+    }
+}
