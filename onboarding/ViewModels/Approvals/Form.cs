5c139f138ecf85efb45eea965f4ffe8a0c04b099
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-05 14:13:33 -0300

    Add pendencies for form enrollment step view model.

---
+++ onboarding/ViewModels/Approvals/Form.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace onboarding.ViewModels.Approvals
+{
+    public class Form
+    {
+        public Form()
+        {
+            Pendencies = new HashSet<Pendency>();
+        }
+
+        public ICollection<Pendency> Pendencies { get; set; }
+    }
+}
