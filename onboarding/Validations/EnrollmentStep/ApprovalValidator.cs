8c8c3d634363abde6b27ff529fa8fcd496452f15
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-05 14:13:59 -0300

    Create approval validator.

---
+++ onboarding/Validations/EnrollmentStep/ApprovalValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using onboarding.Validations.Pendency;
+
+namespace onboarding.Validations.Approval
+{
+    public class EnrollmentStepValidator : AbstractValidator<Models.EnrollmentStep>
+    {
+        public EnrollmentStepValidator()
+        {
+            RuleFor(enrollmentStep => enrollmentStep.Pendencies).SetCollectionValidator(new PendencyValidator());
+        }
+    }
+}
