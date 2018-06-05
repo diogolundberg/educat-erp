d30b60125bd627dac187913dc4c49c1740b8c11e
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-05 14:13:22 -0300

    Add pendencies for record enrollment step view model.

--- onboarding/ViewModels/Approvals/Record.cs
+++ onboarding/ViewModels/Approvals/Record.cs
@@ -1,10 +1,19 @@
-﻿namespace onboarding.ViewModels.Approvals
+﻿using System.Collections.Generic;
+
+namespace onboarding.ViewModels.Approvals
 {
     public class Record
     {
+        public Record()
+        {
+            Pendencies = new HashSet<Pendency>();
+        }
+
         public int Id { get; set; }
         public string SentAt { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+
+        public ICollection<Pendency> Pendencies { get; set; }
     }
 }
