07d1355282f8b00f42b45c81732ff2c6dd86835c
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-05 14:15:17 -0300

    Add mapper for form enrollment step.

--- onboarding/Bindings/ApprovalProfile.cs
+++ onboarding/Bindings/ApprovalProfile.cs
@@ -16,7 +16,11 @@ namespace onboarding.Bindings
             CreateMap<EnrollmentStep, Record>()
                 .ForMember(x => x.SentAt, config => config.MapFrom(x => x.Enrollment.SentAt.Format()))
                 .ForMember(x => x.Name, config => config.MapFrom(x => x.Enrollment.PersonalData.RealName))
-                .ForMember(x => x.Email, config => config.MapFrom(x => x.Enrollment.PersonalData.Email));
+                .ForMember(x => x.Email, config => config.MapFrom(x => x.Enrollment.PersonalData.Email))
+                .ForMember(x => x.Pendencies, config => config.MapFrom(x => x.Pendencies));
+
+            CreateMap<Form, EnrollmentStep>()
+                .ForMember(x => x.Pendencies, config => config.MapFrom(x => x.Pendencies));
         }
     }
 }
