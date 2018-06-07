852234bdbaef70e5ceeec1bcf8e922c764d84bf0
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-07 18:18:09 -0300

    Create step status class.

---
+++ onboarding/Statuses/StepStatus.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using onboarding.Models;
+
+namespace onboarding.Statuses
+{
+    public class StepStatus : BaseStatus<Step>
+    {
+        private readonly EnrollmentStep _enrollmentStep;
+        public StepStatus(AbstractValidator<Step> validator, Step entity, EnrollmentStep enrollmentStep) : base(validator, entity)
+        {
+            _enrollmentStep = enrollmentStep;
+        }
+
+        public override string GetStatus()
+        {
+            if(_enrollmentStep == null)
+            {
+                return "empty";
+            }
+            else if(_enrollmentStep != null && _enrollmentStep.Pendencies.Count > 0)
+            {
+                return "pending";
+            }
+            else
+            {
+                return "valid";
+            }
+        }
+    }
+}
