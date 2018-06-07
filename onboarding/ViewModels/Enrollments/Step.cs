08d4f7ac762e689ffd70c20f2914f8d10c8c0306
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-07 18:11:33 -0300

    Add pendency view model into step view model.

--- onboarding/ViewModels/Enrollments/Step.cs
+++ onboarding/ViewModels/Enrollments/Step.cs
@@ -1,9 +1,17 @@
-﻿namespace onboarding.ViewModels.Enrollments
+﻿using System.Collections.Generic;
+
+namespace onboarding.ViewModels.Enrollments
 {
     public class Step
     {
+        public Step()
+        {
+            Pendencies = new HashSet<Pendency>();
+        }
+
         public string Name { get; set; }
         public string Resource { get; set; }
         public string Status { get; set; }
+        public IEnumerable<Pendency> Pendencies { get; set; }
     }
 }
