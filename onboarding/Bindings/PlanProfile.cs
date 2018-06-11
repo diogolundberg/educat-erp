a4fd2cef9023dbca8d8f277c75205fa7d08d6ccc
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-11 12:40:45 -0300

    Create profile.

---
+++ onboarding/Bindings/PlanProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using onboarding.Models;
+using onboarding.ViewModels.FinanceDatas;
+
+namespace onboarding.Bindings
+{
+    public class PlanProfile : Profile
+    {
+        public PlanProfile()
+        {
+            CreateMap<PlanViewModel, Plan>().ReverseMap();
+        }
+    }
+}
