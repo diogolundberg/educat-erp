ea8b224a9deca6bc6fa5271925d8fc7e6d01bf90
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-11 12:40:29 -0300

    add plan view model.

---
+++ onboarding/ViewModels/FinanceDatas/PlanViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace onboarding.ViewModels.FinanceDatas
+{
+    public class PlanViewModel
+    {
+        public string Name { get; set; }
+        public string Installments { get; set; }
+        public string DueDate { get; set; }
+        public string Value { get; set; }
+        public string InstallmentValue { get; set; }
+    }
+}
