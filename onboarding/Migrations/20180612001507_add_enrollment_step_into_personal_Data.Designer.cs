0332ad5c45c35f17870a3c8aba48f0149c6fc57d
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-11 21:21:31 -0300

    Add migration for create enrollment step relationship with personal data.

--- onboarding/Migrations/20180612001507_add_enrollment_step_into_personal_Data.Designer.cs
+++ onboarding/Migrations/20180612001507_add_enrollment_step_into_personal_Data.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace onboarding.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180612001507_add_enrollment_step_into_personal_Data")]
+    partial class add_enrollment_step_into_personal_Data
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ onboarding/Migrations/20180612001507_add_enrollment_step_into_personal_Data.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace onboarding.Migrations
+{
+    public partial class add_enrollment_step_into_personal_Data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EnrollmentStepId",
+                table: "PersonalDatas",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonalDatas_EnrollmentStepId",
+                table: "PersonalDatas",
+                column: "EnrollmentStepId",
+                unique: true,
+                filter: "[EnrollmentStepId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PersonalDatas_EnrollmentSteps_EnrollmentStepId",
+                table: "PersonalDatas",
+                column: "EnrollmentStepId",
+                principalTable: "EnrollmentSteps",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PersonalDatas_EnrollmentSteps_EnrollmentStepId",
+                table: "PersonalDatas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PersonalDatas_EnrollmentStepId",
+                table: "PersonalDatas");
+
+            migrationBuilder.DropColumn(
+                name: "EnrollmentStepId",
+                table: "PersonalDatas");
+        }
+    }
+}
