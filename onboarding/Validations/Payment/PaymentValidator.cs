ff5ac2b46b618815692de02fbd1dded5640f4116
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-06-13 17:49:18 -0300

    Create validator for payment.

---
+++ onboarding/Validations/Payment/PaymentValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace onboarding.Validations.Payment
+{
+    public class PaymentValidator : AbstractValidator<Models.Payment>
+    {
+
+        public PaymentValidator()
+        {
+            RuleFor(contract => contract.BilletUrl).NotEmpty().WithName(Resources.Models.Payment.BilletUrl);
+            RuleFor(contract => contract.Url).NotEmpty().WithName(Resources.Models.Payment.Url);
+        }
+    }
+}
