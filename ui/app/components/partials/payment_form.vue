e8e565e2fca4a434e6f96895f2312ac0ad34ad05
Silvio Henrique Ferreira <shferreira@me.com>
2018-06-16 15:39:42 -0300

    Add fieldset to the payment form

--- ui/app/components/partials/payment_form.vue
+++ ui/app/components/partials/payment_form.vue
@@ -6,21 +6,23 @@
       :sub-key="['data']"
       post>
       <template slot-scope="{ item, errors }">
-        <Alert
-          v-for="(pendency, index) in item.pendencies"
-          :key="index"
-          :message="pendency.description"
-          error />
-        <div>
-          <a
-            :href="item.billetUrl"
-            class="btn btn-primary upcase">
-            Baixar boleto para pagamento
-          </a>
-          <UploadButton
-            v-model="item.url"
-            label="Enviar Comprovante de Pagamento" />
-        </div>
+        <Fieldset title="Boleto">
+          <Alert
+            v-for="(pendency, index) in item.pendencies"
+            :key="index"
+            :message="pendency.description"
+            error />
+          <div>
+            <a
+              :href="item.billetUrl"
+              class="btn btn-primary upcase">
+              Baixar boleto para pagamento
+            </a>
+            <UploadButton
+              v-model="item.url"
+              label="Enviar Comprovante de Pagamento" />
+          </div>
+        </Fieldset>
       </template>
     </DataForm>
   </div>
